fe1d8e976c1464d22fb85001eb0a57a558edebb2
Ben Lovell <benjamin.lovell@gmail.com>
2014-12-19 15:51:14 +0000

    Completes happy path for contacts
    * Switches `create` to a regular form handler for now

---
+++ spec/features/principal_contacts_administrator_spec.rb
@@ -0,0 +1,33 @@
+RSpec.feature 'Principal contacts Administrator' do
+  let(:rejection_page) { RejectionPage.new }
+
+  scenario 'Sending a valid message' do
+    given_i_have_failed_pre_qualification
+    when_i_submit_my_message
+    then_my_message_is_sent_to_the_administrator
+    and_i_am_notified
+  end
+
+  scenario 'Attempting to send an invalid message'
+
+
+  def given_i_have_failed_pre_qualification
+    rejection_page.load
+  end
+
+  def when_i_submit_my_message
+    rejection_page.tap do |p|
+      p.principal_email.set 'dave@example.com'
+      p.administrator_message.set 'Send help'
+      p.send_message.click
+    end
+  end
+
+  def then_my_message_is_sent_to_the_administrator
+    expect(ActionMailer::Base.deliveries).not_to be_empty
+  end
+
+  def and_i_am_notified
+    expect(rejection_page).to have_content('Your message has been sent')
+  end
+end
--- spec/support/rejection_page.rb
+++ spec/support/rejection_page.rb
@@ -2,6 +2,9 @@ class RejectionPage < SitePrism::Page
   set_url '/principal/reject'
   set_url_matcher /reject/
 
+  element :principal_email, '.t-email'
   element :administrator_message, '.t-message'
+  element :send_message, '.button--primary'
+
   element :go_back, '.t-back'
 end
