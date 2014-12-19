c0a444be8921207a850c1cb3a6affe1e5031aef0
Ben Lovell <benjamin.lovell@gmail.com>
2014-12-19 15:05:23 +0000

    Adds `from` address for the mailing
    * Also tidies up the `contact` body a little

--- app/mailers/admin_contact.rb
+++ app/mailers/admin_contact.rb
@@ -1,9 +1,11 @@
 class AdminContact < ActionMailer::Base
+  default(
+    from: 'IFADirectoryQueries@moneyadviceservice.org.uk',
+    to: 'IFADirectoryQueries@moneyadviceservice.org.uk'
+  )
+
   def contact(email, message)
     @email, @message = email, message
-    mail(
-      to: 'IFADirectoryQueries@moneyadviceservice.org.uk',
-      subject: 'IFA Contact'
-    )
+    mail subject: 'IFA Contact'
   end
 end
