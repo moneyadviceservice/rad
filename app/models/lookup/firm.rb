b5d919a3ca20556386b28b189dcceee52bf79914
Ben Lovell <benjamin.lovell@gmail.com>
2014-12-21 12:55:29 +0000

    Adds spec and validation for FCA Number

--- app/models/lookup/firm.rb
+++ app/models/lookup/firm.rb
@@ -1,5 +1,9 @@
 module Lookup
   class Firm < ActiveRecord::Base
+    validates :fca_number,
+      length: { is: 6 },
+      numericality: { only_integer: true }
+
     def self.table_name
       "lookup_#{super}"
     end
