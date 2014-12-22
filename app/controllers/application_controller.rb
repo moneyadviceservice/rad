14a9047f1141c3542114c42c8c2c97e5583c2f3c
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2014-12-22 15:08:10 +0000

    Simple auth for Principal using the token.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :authenticate
+
   def display_search_box_in_header?
     false
   end
@@ -26,4 +28,20 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :display_adviser_sign_in?
+
+  private
+
+  def authenticate
+    authenticate_token or redirect_to(error_path) unless current_user
+  end
+
+  def current_user
+    @current_user ||= Principal.find_by!(id: session[:user_id]) if session.key?(:user_id)
+  end
+
+  def authenticate_token
+    @current_user = Principal.authenticate(params[:token]) do |principal|
+      session[:user_id] = principal.id
+    end
+  end
 end
--- app/models/principal.rb
+++ app/models/principal.rb
@@ -1,6 +1,15 @@
 class Principal < ActiveRecord::Base
   before_create :generate_token
 
+  def self.authenticate(token)
+    principal = self.find_by(token: token)
+    if principal
+      principal.touch(:last_sign_in_at)
+      yield(principal)
+      principal
+    end
+  end
+
   private
 
   def generate_token
