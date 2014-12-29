162808c61b76d0006eb10586a36227900057af4c
Ben Lovell <benjamin.lovell@gmail.com>
2014-12-29 11:27:27 +0000

    Removes redundant sidekiq job for identification
    Superseded by ActiveJob

--- spec/workers/identification_email_worker_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe IdentificationEmailWorker do
-  let(:principal) { create(:principal) }
-
-  subject { described_class.new.perform(principal.id) }
-
-  it 'delivers an email' do
-    expect { subject }.to change { ActionMailer::Base.deliveries }
-  end
-end
