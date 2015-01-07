01eef6f166c238de6778396fbe558dff3cf1ea7d
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-07 09:58:07 +0200

    Lines 1 and 2 of the main office address to the questionnaire form model.

--- app/forms/questionnaire_step1_form.rb
+++ app/forms/questionnaire_step1_form.rb
@@ -1,7 +1,7 @@
 class QuestionnaireStep1Form
   include ActiveModel::Model
 
-  attr_accessor :firm_email_address, :firm_telephone_number
+  attr_accessor :firm_email_address, :firm_telephone_number, :main_office_line_1, :main_office_line_2
 
   validates :firm_email_address,
             presence: true,
@@ -12,4 +12,11 @@ class QuestionnaireStep1Form
             presence: true,
             length: {maximum: 30},
             format: {with: /\A[0-9 ]+\z/}
+
+  validates :main_office_line_1,
+            presence: true,
+            length: {maximum: 100}
+
+  validates :main_office_line_2,
+            presence: true
 end
--- spec/forms/questionnaire_step1_form_spec.rb
+++ spec/forms/questionnaire_step1_form_spec.rb
@@ -1,11 +1,15 @@
 RSpec.describe QuestionnaireStep1Form, '#valid?', type: :model do
   let(:firm_email_address) { 'info@finsmart.com' }
   let(:firm_telephone_number) { '0300 500 5000' }
+  let(:main_office_line_1) { 'Holborn Centre' }
+  let(:main_office_line_2) { '120 Holborn' }
 
   subject do
     described_class.new({
       firm_email_address: firm_email_address,
-      firm_telephone_number: firm_telephone_number
+      firm_telephone_number: firm_telephone_number,
+      main_office_line_1: main_office_line_1,
+      main_office_line_2: main_office_line_2
     })
   end
 
@@ -36,6 +40,22 @@ RSpec.describe QuestionnaireStep1Form, '#valid?', type: :model do
 
     context 'when invalid' do
       let(:firm_telephone_number) { 'invalid-telephone-number' }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
+
+  describe 'main office line 1' do
+    context 'when missing' do
+      let(:main_office_line_1) { nil }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
+
+  describe 'main office line 2' do
+    context 'when missing' do
+      let(:main_office_line_2) { nil }
 
       it { is_expected.not_to be_valid }
     end
