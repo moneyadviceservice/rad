ed1de1157f6fb1fd6b376748ff20a22a0210d521
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-07 09:38:49 +0200

    Firm's telephone number to questionnaire form model.

--- app/forms/questionnaire_step1_form.rb
+++ app/forms/questionnaire_step1_form.rb
@@ -1,10 +1,15 @@
 class QuestionnaireStep1Form
   include ActiveModel::Model
 
-  attr_accessor :firm_email_address
+  attr_accessor :firm_email_address, :firm_telephone_number
 
   validates :firm_email_address,
             presence: true,
             length: {maximum: 50},
             format: {with: /.+@.+\..+/}
+
+  validates :firm_telephone_number,
+            presence: true,
+            length: {maximum: 30},
+            format: {with: /\A[0-9 ]+\z/}
 end
--- spec/forms/questionnaire_step1_form_spec.rb
+++ spec/forms/questionnaire_step1_form_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe QuestionnaireStep1Form, '#valid?', type: :model do
   let(:firm_email_address) { 'info@finsmart.com' }
+  let(:firm_telephone_number) { '0300 500 5000' }
 
   subject do
     described_class.new({
-      firm_email_address: firm_email_address
+      firm_email_address: firm_email_address,
+      firm_telephone_number: firm_telephone_number
     })
   end
 
@@ -20,6 +22,20 @@ RSpec.describe QuestionnaireStep1Form, '#valid?', type: :model do
 
     context 'when invalid' do
       let(:firm_email_address) { 'invalid-email-format' }
+
+      it { is_expected.not_to be_valid }
+    end
+  end
+
+  describe 'firm telephone number' do
+    context 'when missing' do
+      let(:firm_telephone_number) { nil }
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context 'when invalid' do
+      let(:firm_telephone_number) { 'invalid-telephone-number' }
 
       it { is_expected.not_to be_valid }
     end
