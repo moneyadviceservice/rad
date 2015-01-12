aa80c1e77a841b4f7a84dff4d1c417c47642ecef
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-12 11:49:47 +0200

    Improves formatting around braces used in validation definitions.

--- app/forms/questionnaire_step_one_form.rb
+++ app/forms/questionnaire_step_one_form.rb
@@ -20,21 +20,21 @@ class QuestionnaireStepOneForm
 
   validates :firm_email_address,
             presence: true,
-            length: {maximum: 50},
-            format: {with: /.+@.+\..+/}
+            length: { maximum: 50 },
+            format: { with: /.+@.+\..+/ }
 
   validates :firm_telephone_number,
             presence: true,
-            length: {maximum: 30},
-            format: {with: /\A[0-9 ]+\z/}
+            length: { maximum: 30},
+            format: { with: /\A[0-9 ]+\z/ }
 
   validates :main_office_line_1,
             presence: true,
-            length: {maximum: 100}
+            length: { maximum: 100 }
 
   validates :main_office_postcode,
             presence: true,
-            format: {with: /\A[a-zA-Z\d]{1,4} [a-zA-Z\d]{1,3}\z/}
+            format: { with: /\A[a-zA-Z\d]{1,4} [a-zA-Z\d]{1,3}\z/ }
 
   validates :main_office_line_2,
             :main_office_town,
