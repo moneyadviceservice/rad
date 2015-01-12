42a6ab7af935d00cf763dbc751fee441268d8335
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-01-12 14:02:24 +0000

    Refactor the pre-qual questions to use dough helpers

--- app/forms/pre_qualification_form.rb
+++ app/forms/pre_qualification_form.rb
@@ -1,7 +1,7 @@
 class PreQualificationForm
   include ActiveModel::Model
 
-  attr_accessor :question_1, :question_2, :question_3
+  attr_accessor :question_1, :question_2, :question_3, :question_4, :question_5
 
-  validates :question_1, :question_2, :question_3, inclusion: { in: %w{1} }
+  validates :question_1, :question_2, :question_3, :question_4, :question_5, inclusion: { in: %w{1} }
 end
