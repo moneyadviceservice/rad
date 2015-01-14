953f1fd2c240cb555d7160dd515c9bb1a91815a0
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-14 16:39:51 +0000

    Adds "new" and "create" questionnaire routes.

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,9 @@ Rails.application.routes.draw do
     end
 
     resource :firm, only: :show do
-      resource :questionnaire
-
+      resource :questionnaire, only: [:new, :create]
       resources :advisers
     end
-
   end
 
   resource :contact, only: :create
