2de2e6f286ce7fb0435955deb4e28e8211fd8d8b
Ben Lovell <benjamin.lovell@gmail.com>
2015-01-14 15:38:31 +0000

    Completes Adviser import validation
    * Adds reference number validation
    * Adds simple name validation

--- app/models/lookup/adviser.rb
+++ app/models/lookup/adviser.rb
@@ -1,5 +1,8 @@
 module Lookup
   class Adviser < ActiveRecord::Base
+    validates_length_of   :reference_number, is: 8
+    validates_presence_of :name
+
     def self.table_name
       "lookup_#{super}"
     end
