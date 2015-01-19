4cc4982245888caa9d54d87b8e6afce870fee6cc
Ben Lovell <benjamin.lovell@gmail.com>
2015-01-19 14:43:10 +0000

    Stylistic changes to `let` hashes

--- spec/forms/pre_qualification_form_spec.rb
+++ spec/forms/pre_qualification_form_spec.rb
@@ -4,41 +4,32 @@ RSpec.describe PreQualificationForm, '#valid?', type: :model do
   subject { described_class.new(params) }
 
   context 'when only question 4 is answered' do
-    let(:params) do
-      {
-        question_4: '1'
-      }
-    end
+    let(:params) {{ question_4: '1' }}
 
     it { is_expected.not_to be_valid }
   end
 
   context 'when one of the answers is missing' do
-    let(:params) do {
-      question_1: '1',
-      question_2: '1'
-    } end
+    let(:params) {{ question_1: '1', question_2: '1' }}
 
     it { is_expected.not_to be_valid }
   end
 
   context 'when one or more of the answers is no' do
-    let(:params) do {
-      question_1: '0',
-      question_2: '1',
-      question_3: '1',
-    } end
+    let(:params) {{ question_1: '0', question_2: '1', question_3: '1' }}
 
     it { is_expected.not_to be_valid }
   end
 
   context 'when all answers are yes' do
-    let(:params) do {
-      question_1: '1',
-      question_2: '1',
-      question_3: '1',
-      question_4: '1'
-    } end
+    let(:params) do
+      {
+        question_1: '1',
+        question_2: '1',
+        question_3: '1',
+        question_4: '1'
+      }
+    end
 
     it { is_expected.to be_valid }
   end
