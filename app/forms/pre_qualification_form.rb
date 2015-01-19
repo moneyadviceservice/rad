0e0cf3969a0468d8fbd5a7afbfc04dabf9d6978b
Ben Lovell <benjamin.lovell@gmail.com>
2015-01-19 11:40:59 +0000

    Makes validations slightly more concise

--- app/forms/pre_qualification_form.rb
+++ app/forms/pre_qualification_form.rb
@@ -3,11 +3,11 @@ class PreQualificationForm
 
   attr_accessor :question_1, :question_2, :question_3, :question_4, :question_5
 
-  validates :question_1, :question_2, :question_3, inclusion: { in: ['1'] }
+  validates :question_1, :question_2, :question_3, acceptance: true
 
-  validates :question_4, inclusion: { in: ['0', '1'] }
+  validates :question_4, inclusion: { in: %w(0 1) }
 
-  validates :question_5, inclusion: { in: ['1'] }, if: -> { self.restricted? }
+  validates :question_5, acceptance: true, if: -> { self.restricted? }
 
   def restricted?
     question_4 == '0'
