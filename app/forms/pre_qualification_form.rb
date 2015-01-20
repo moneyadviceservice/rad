8315ec55a15e41188cc398623c66ab74bfe78f20
Ben Lovell <benjamin.lovell@gmail.com>
2015-01-20 16:52:41 +0000

    Minor stylistic changes

--- app/forms/pre_qualification_form.rb
+++ app/forms/pre_qualification_form.rb
@@ -1,13 +1,22 @@
 class PreQualificationForm
   include ActiveModel::Model
 
-  attr_accessor :active_question, :business_model_question, :status_question, :particular_market_question, :consider_available_providers_question
+  attr_accessor :active_question,
+    :business_model_question,
+    :status_question,
+    :particular_market_question,
+    :consider_available_providers_question
 
-  validates :active_question, :business_model_question, acceptance: true, presence: true
+  validates :active_question, :business_model_question,
+    acceptance: true,
+    presence: true
 
   validates :status_question, inclusion: { in: %w(0 1) }
 
-  validates :particular_market_question, presence: true, acceptance: true, if: -> { self.restricted? }
+  validates :particular_market_question,
+    presence: true,
+    acceptance: true,
+    if: -> { self.restricted? }
 
   def restricted?
     status_question == '0'
