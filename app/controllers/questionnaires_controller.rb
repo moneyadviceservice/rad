5a5da07380379eb44da324778003c3e215aee106
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-20 12:24:50 +0000

    Adds "e-mail address" and "phone number" to the firm questionnaire.

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -3,4 +3,26 @@ class QuestionnairesController < ApplicationController
     @lookup_firm = current_user.firm
     @firm = Firm.new
   end
+
+  def create
+    @firm = Firm.new(firm_params)
+
+    if @firm.valid?
+      render nothing: true
+    else
+      @lookup_firm = current_user.firm
+
+      render :new
+    end
+  end
+
+  private
+
+  def firm_params
+    params.require(:firm)
+    .permit(
+        :email_address,
+        :telephone_number,
+    )
+  end
 end
