a3e9c33f070f203fb2a9e3acf0b3b8e080dcd18f
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-20 13:33:55 +0000

    Basic feature spec for the firm questionnaire.

--- spec/support/questionnaire_page.rb
+++ spec/support/questionnaire_page.rb
@@ -1,4 +1,12 @@
 class QuestionnairePage < SitePrism::Page
-  set_url 'principals/{principal}/firm/questionnaire'
-  set_url_matcher %r{/principals/[a-f0-9]{8}/firm/questionnaire}
+  set_url 'principals/{principal}/firm/questionnaire/new'
+  set_url_matcher %r{/principals/[a-f0-9]{8}/firm/questionnaire(/new)?}
+
+  element :firm_name, '.t-firm-name'
+  element :firm_fca_number, '.t-firm-fca-number'
+
+  element :email_address_field, '#firm_email_address'
+  element :telephone_number_field, '#firm_telephone_number'
+
+  element :next_button, '.l-questionnaire__button button'
 end
