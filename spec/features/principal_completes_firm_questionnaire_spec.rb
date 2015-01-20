910a132e42f68e6b8ac2a290c66a49d0115a2446
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-20 13:33:55 +0000

    Basic feature spec for the firm questionnaire.

---
+++ spec/features/principal_completes_firm_questionnaire_spec.rb
@@ -0,0 +1,35 @@
+RSpec.feature 'Principal completes the firm questionnaire' do
+  let(:questionnaire_page) { QuestionnairePage.new }
+
+  scenario 'Successfully complete the questionnaire' do
+    given_i_have_selected_a_firm
+    and_i_can_see_my_firm_name_and_fca_reference_number
+    when_I_complete_all_mandatory_questions
+    then_my_directory_listing_is_created_and_marked_as_done
+    and_i_am_directed_to_assign_advisers_to_my_firm_or_subsidiary
+  end
+
+  def given_i_have_selected_a_firm
+    @principal = create(:principal)
+    questionnaire_page.load(principal: @principal.token)
+  end
+
+  def and_i_can_see_my_firm_name_and_fca_reference_number
+    expect(questionnaire_page.firm_name.text).to eql(@principal.firm.registered_name)
+    expect(questionnaire_page.firm_fca_number.text).to eql(@principal.firm.fca_number.to_s)
+  end
+
+  def when_I_complete_all_mandatory_questions
+    questionnaire_page.email_address_field.set(Faker::Internet.email)
+    questionnaire_page.telephone_number_field.set(Faker::Base.numerify('##### ### ###'))
+    questionnaire_page.next_button.click
+  end
+
+  def then_my_directory_listing_is_created_and_marked_as_done
+    skip('waiting on dependant story')
+  end
+
+  def and_i_am_directed_to_assign_advisers_to_my_firm_or_subsidiary
+    skip('waiting on dependant story')
+  end
+end
