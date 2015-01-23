c668bc8994555c34f6966997d96bb5f2c1513e3e
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-23 13:12:44 +0000

    Improves formatting for firm model validation declarations.

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -13,79 +13,84 @@ class Firm < ActiveRecord::Base
   attr_accessor :percent_total
 
   validates :email_address,
-            presence: true,
-            length: { maximum: 50 },
-            format: { with: /.+@.+\..+/ }
+    presence: true,
+    length: { maximum: 50 },
+    format: { with: /.+@.+\..+/ }
 
   validates :telephone_number,
-            presence: true,
-            length: { maximum: 30 },
-            format: { with: /\A[0-9 ]+\z/ }
+    presence: true,
+    length: { maximum: 30 },
+    format: { with: /\A[0-9 ]+\z/ }
 
   validates :address_line_1,
-            presence: true,
-            length: { maximum: 100 }
+    presence: true,
+    length: { maximum: 100 }
 
   validates :address_line_2,
-            length: { maximum: 100 }
+    length: { maximum: 100 }
 
   validates :address_postcode,
-            presence: true,
-            format: { with: /\A[a-zA-Z\d]{1,4} [a-zA-Z\d]{1,3}\z/ }
+    presence: true,
+    format: { with: /\A[a-zA-Z\d]{1,4} [a-zA-Z\d]{1,3}\z/ }
 
   validates :address_town,
-            :address_county,
-            presence: true
+    :address_county,
+    presence: true
 
   validates :in_person_advice_methods,
-            length: { minimum: 1 }
+    length: { minimum: 1 }
 
   validates :free_initial_meeting,
-            inclusion: { in: [true, false] }
+    inclusion: { in: [true, false] }
 
   validates :initial_meeting_duration,
-            presence: true,
-            if: ->{ free_initial_meeting }
+    presence: true,
+    if: ->{ free_initial_meeting }
 
   validates :initial_advice_fee_structures,
-            length: { minimum: 1 }
+    length: { minimum: 1 }
 
   validates :ongoing_advice_fee_structures,
-            length: { minimum: 1 }
+    length: { minimum: 1 }
 
   validates :allowed_payment_methods,
-            length: { minimum: 1 }
+    length: { minimum: 1 }
 
   validates :minimum_fixed_fee,
-            allow_blank: true,
-            numericality: { only_integer: true }
+    allow_blank: true,
+    numericality: { only_integer: true }
 
   validate :sum_of_percentages_equals_one_hundred
 
   validates :retirement_income_products_percent,
-            :pension_transfer_percent,
-            :long_term_care_percent,
-            :equity_release_percent,
-            :inheritance_tax_and_estate_planning_percent,
-            :wills_and_probate_percent,
-            :other_percent,
-            presence: true,
-            numericality: { only_integer: true }
+    :pension_transfer_percent,
+    :long_term_care_percent,
+    :equity_release_percent,
+    :inheritance_tax_and_estate_planning_percent,
+    :wills_and_probate_percent,
+    :other_percent,
+    presence: true,
+    numericality: { only_integer: true }
 
   validates :investment_sizes,
-            length: { minimum: 1 }
+    length: { minimum: 1 }
 
   private
 
   def sum_of_percentages_equals_one_hundred
     total = retirement_income_products_percent.to_i \
-          + pension_transfer_percent.to_i \
-          + long_term_care_percent.to_i \
-          + equity_release_percent.to_i \
-          + inheritance_tax_and_estate_planning_percent.to_i \
-          + wills_and_probate_percent.to_i \
-          + other_percent.to_i
+      + pension_transfer_percent.to_i \
+      + long_term_care_percent.to_i \
+      + equity_release_percent.to_i \
+      + inheritance_tax_and_estate_planning_percent.to_i \
+      + wills_and_probate_percent.to_i \
+      + other_percent.to_i
 
-    errors.add(:percent_total, I18n.t('questionnaire.retirement_advice.percent_total.not_one_hundred')) unless total == 100
+    unless total == 100
+      errors.add(
+          :percent_total,
+          I18n.t('questionnaire.retirement_advice.percent_total.not_one_hundred')
+      )
+    end
   end
 end
