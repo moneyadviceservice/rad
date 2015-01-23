663aa0c009fb0c0b3898699440effdfc2ded137b
Ben Lovell <benjamin.lovell@gmail.com>
2015-01-23 15:47:15 +0000

    Adds leading slash to URL

--- spec/support/adviser_page.rb
+++ spec/support/adviser_page.rb
@@ -1,5 +1,5 @@
 class AdviserPage < SitePrism::Page
-  set_url 'principals/{principal}/firm/advisers/new'
+  set_url '/principals/{principal}/firm/advisers/new'
   set_url_matcher %r{/principals/[a-f0-9]{8}/firm/advisers/new}
 
   element :reference_number, '.t-reference-number'
--- spec/support/firm_page.rb
+++ spec/support/firm_page.rb
@@ -1,5 +1,5 @@
 class FirmPage < SitePrism::Page
-  set_url 'principals/{principal}/firm'
+  set_url '/principals/{principal}/firm'
   set_url_matcher %r{/principals/[a-f0-9]{8}/firm}
 
   element :firm_title, '.t-firm-title'
