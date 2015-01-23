afa4acfa20875b590a30bf2b00fd5c561de0fb39
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-23 13:02:09 +0000

    Neatens questionnaire feature spec.

--- spec/features/principal_completes_firm_questionnaire_spec.rb
+++ spec/features/principal_completes_firm_questionnaire_spec.rb
@@ -3,13 +3,13 @@ RSpec.feature 'Principal completes the firm questionnaire' do
   let(:adviser_page) { AdviserPage.new }
 
   before do
-    FactoryGirl.create(:in_person_advice_method)
-    FactoryGirl.create(:other_advice_method)
-    FactoryGirl.create(:initial_meeting_duration)
-    FactoryGirl.create(:initial_advice_fee_structure)
-    FactoryGirl.create(:ongoing_advice_fee_structure)
-    FactoryGirl.create(:allowed_payment_method)
-    FactoryGirl.create(:investment_size)
+    create(:in_person_advice_method)
+    create(:other_advice_method)
+    create(:initial_meeting_duration)
+    create(:initial_advice_fee_structure)
+    create(:ongoing_advice_fee_structure)
+    create(:allowed_payment_method)
+    create(:investment_size)
   end
 
   scenario 'Successfully complete the questionnaire' do
@@ -31,33 +31,36 @@ RSpec.feature 'Principal completes the firm questionnaire' do
   end
 
   def when_I_complete_all_mandatory_questions
-    questionnaire_page.email_address_field.set(Faker::Internet.email)
-    questionnaire_page.telephone_number_field.set(Faker::Base.numerify('##### ### ###'))
-    questionnaire_page.address_line_1_field.set(Faker::Address.street_address)
-    questionnaire_page.address_town_field.set(Faker::Address.city)
-    questionnaire_page.address_county_field.set(Faker::Address.county)
-    questionnaire_page.address_postcode_field.set(Faker::Address.postcode)
+    questionnaire_page.tap do |p|
+      p.email_address.set Faker::Internet.email
+      p.telephone_number.set Faker::Base.numerify('##### ### ###')
+      p.address_line_1.set Faker::Address.street_address
+      p.address_town.set Faker::Address.city
+      p.address_county.set Faker::Address.county
+      p.address_postcode.set Faker::Address.postcode
 
-    questionnaire_page.in_person_advice_method_checkboxes.first.set(true)
-    questionnaire_page.other_advice_method_checkboxes.first.set(true)
-    questionnaire_page.offers_free_initial_meeting_radio_button.set(true)
-    questionnaire_page.initial_meeting_duration_checkboxes.first.set(true)
-    questionnaire_page.initial_fee_structure_checkboxes.first.set(true)
-    questionnaire_page.ongoing_fee_structure_checkboxes.first.set(true)
-    questionnaire_page.allowed_payment_method_checkboxes.first.set(true)
-    questionnaire_page.minimum_fee_field.set(Faker::Number.number(4))
+      p.in_person_advice_methods.first.set true
+      p.other_advice_methods.first.set true
+      p.offers_free_initial_meeting.set true
+      p.initial_meeting_durations.first.set true
+      p.initial_fee_structures.first.set true
+      p.ongoing_fee_structures.first.set true
+      p.allowed_payment_methods.first.set true
 
-    questionnaire_page.firm_retirement_income_products_percent_field.set(15)
-    questionnaire_page.firm_pension_transfer_percent_field.set(15)
-    questionnaire_page.firm_long_term_care_percent_field.set(15)
-    questionnaire_page.firm_equity_release_percent_field.set(15)
-    questionnaire_page.firm_inheritance_tax_and_estate_planning_percent_field.set(15)
-    questionnaire_page.firm_wills_and_probate_percent_field.set(15)
-    questionnaire_page.firm_other_percent_field.set(10)
+      p.minimum_fee.set Faker::Number.number(4)
 
-    questionnaire_page.investment_size_checkboxes.first.set(true)
+      p.retirement_income_products_percent.set 15
+      p.pension_transfer_percent.set 15
+      p.long_term_care_percent.set 15
+      p.equity_release_percent.set 15
+      p.inheritance_tax_and_estate_planning_percent.set 15
+      p.wills_and_probate_percent.set 15
+      p.other_percent.set 10
 
-    questionnaire_page.next_button.click
+      p.investment_sizes.first.set true
+
+      p.next_button.click
+    end
   end
 
   def then_my_directory_listing_is_created_and_marked_as_done
