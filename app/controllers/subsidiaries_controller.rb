5de7c3cabc9df942c91935e2b25a488231b3c49b
Ben Lovell <benjamin.lovell@gmail.com>
2015-01-25 17:03:41 +0000

    Refactors conversion of subsidiaries
    Just push the logic into `Principal`. Not entirely happy about the
    whole disabling validations thing. Will revisit soon.

--- app/controllers/subsidiaries_controller.rb
+++ app/controllers/subsidiaries_controller.rb
@@ -1,12 +1,6 @@
 class SubsidiariesController < ApplicationController
   def convert
-    subsidiary = current_user.lookup_firm.subsidiaries.find(params[:id])
-    firm = current_user.firm.subsidiaries.find_or_initialize_by(
-      registered_name: subsidiary.name,
-      fca_number: subsidiary.fca_number
-    )
-
-    firm.save(validate: false) unless firm.persisted?
+    firm = current_user.find_or_create_subsidiary(params[:id])
 
     redirect_to edit_principal_firm_questionnaire_path(current_user, firm)
   end
