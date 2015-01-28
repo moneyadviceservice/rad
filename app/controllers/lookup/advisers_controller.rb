0641b1522119510bbb1b802788acc9c5f6124813
Ben Lovell <benjamin.lovell@gmail.com>
2015-01-28 09:59:16 +0000

    Minor refactorings

--- app/controllers/lookup/advisers_controller.rb
+++ app/controllers/lookup/advisers_controller.rb
@@ -1,29 +1,41 @@
 module Lookup
   class AdvisersController < ApplicationController
     def show
-      @adviser = ::Adviser.find_by(reference_number: params[:id])
-
-      if @adviser
-        stat :already_matched
-
-        render json: { error: t('questionnaire.adviser.error_responses.already_exists') }, status: 409
+      if @adviser = ::Adviser.find_by(reference_number: params[:id])
+        errors_for :conflict
       else
-        @lookup_adviser = Adviser.find_by(reference_number: params[:id])
-
-        if @lookup_adviser
-          stat :matched
-
-          render json: { name: @lookup_adviser.name }
+        if @lookup_adviser = Adviser.find_by(reference_number: params[:id])
+          json_for @lookup_adviser
         else
-          stat :unmatched
-
-          render json: { error: t('questionnaire.adviser.error_responses.not_found') }, status: 404
+          errors_for :not_found
         end
       end
     end
 
+    private
+
     def stat(key)
       Stats.increment("radsignup.adviser.#{key}")
+    end
+
+    def json_for(lookup_adviser)
+      stat :matched
+
+      render json: { name: lookup_adviser.name }
+    end
+
+    def errors_for(status)
+      stat stat_key_for(status)
+
+      render json: { error: t("questionnaire.adviser.error_responses.{status}") }, status: status
+    end
+
+    def stat_key_for(status)
+      if status == :not_found
+        :unmatched
+      else
+        :already_matched
+      end
     end
   end
 end
