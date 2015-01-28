22899eacd00b2a7bf76ed56d19b6f345e10e7ca9
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-01-28 16:21:05 +0000

    Fixes feature spec to work with radio buttons for "covers whole of uk".

--- spec/support/adviser_page.rb
+++ spec/support/adviser_page.rb
@@ -8,6 +8,9 @@ class AdviserPage < SitePrism::Page
   element :confirmed_disclaimer, '.t-confirmed-disclaimer'
   element :submit, '.t-submit'
 
+  element :does_cover_whole_of_uk, '#adviser_covers_whole_of_uk_true'
+  element :does_not_cover_whole_of_uk, '#adviser_covers_whole_of_uk_false'
+
   def adviser_unmatched?
     first(
       '.validation-summary__error',
@@ -17,10 +20,5 @@ class AdviserPage < SitePrism::Page
 
   def matched_adviser?(name)
     has_content?(name)
-  end
-
-  def covers_whole_of_uk(v)
-    key = I18n.t('questionnaire.adviser.geographical_coverage.covers_whole_of_uk_html')
-    v ? check(key) : uncheck(key)
   end
 end
