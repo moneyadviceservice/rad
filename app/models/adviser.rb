fedcaa7b3013000b2963f5b05263145d25fedbca
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-02-04 07:14:47 +0000

    Change postcode field to validate lowercase

--- app/models/adviser.rb
+++ app/models/adviser.rb
@@ -22,7 +22,7 @@ class Adviser < ActiveRecord::Base
 
   validates :postcode,
     presence: true,
-    format: { with: /\A[A-Z\d]{1,4} [A-Z\d]{1,3}\z/ },
+    format: { with: /(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))/ },
     unless: :covers_whole_of_uk?
 
   validates :reference_number,
--- app/models/firm.rb
+++ app/models/firm.rb
@@ -37,7 +37,7 @@ class Firm < ActiveRecord::Base
 
   validates :address_postcode,
     presence: true,
-    format: { with: /\A[A-Z\d]{1,4} [A-Z\d]{1,3}\z/ }
+    format: { with: /(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))/ }
 
   validates :address_town,
     :address_county,
