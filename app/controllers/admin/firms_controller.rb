f550a03d9389f139dcd904ef9a72a6c443783f04
Jon Gilbraith <jon@jon.vc>
2015-02-04 16:41:40 +0000

    Add a firms#show page.

--- app/controllers/admin/firms_controller.rb
+++ app/controllers/admin/firms_controller.rb
@@ -2,4 +2,8 @@ class Admin::FirmsController < Admin::ApplicationController
   def index
     @firms = Firm.page(params[:page]).per(20)
   end
+
+  def show
+    @firm = Firm.find(params[:id])
+  end
 end
