a1599cd11ba724393924e5981363daa510e5f1bd
Jon Gilbraith <jon@jon.vc>
2015-02-09 13:36:14 +0000

    Protect admin area with basic auth.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,4 +4,11 @@ class Admin::ApplicationController < ActionController::Base
 
   layout 'admin'
 
+  before_action :authenticate, if: -> { Authentication.required? }
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      Authentication.authenticate(username, password)
+    end
+  end
 end
