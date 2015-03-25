4b9b59d27ae8ee75ea68263ca4d2a1add79f9b0d
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-03-25 12:10:04 +0000

    Adds ordering to seed data (mostly used for local instances).

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,63 +1,52 @@
-FactoryGirl.create_list(:principal, 3) if Rails.env.development?
-
-[
-  'At customers home',
-  'At firm\'s place of business',
-  'At an agreed location',
-].each { |item| InPersonAdviceMethod.find_or_create_by(name: item) }
-
-[
-  'Advice by telephone',
-  'Advice online (e.g. by video call / conference / email)'
-].each { |item| OtherAdviceMethod.find_or_create_by(name: item) }
-
-[
-  '30 min',
-  '60 min'
-].each { |item| InitialMeetingDuration.find_or_create_by(name: item) }
-
-[
-  'Hourly fee',
-  'Fixed fee',
-  'Other',
-].each { |item| InitialAdviceFeeStructure.find_or_create_by(name: item) }
-
-[
-  'Hourly fee',
-  'Fixed upfront fee',
-  'Monthly by direct debit / standing order',
-  'Other'
-].each { |item| OngoingAdviceFeeStructure.find_or_create_by(name: item) }
-
-[
-  'From their own resources',
-  'From funds to be invested'
-].each { |item| AllowedPaymentMethod.find_or_create_by(name: item) }
-
-[
-  'Under £50,000',
-  '£50,000 - £99,999',
-  '£100,000 - £149,999',
-  'Over £150,000'
-].each { |item| InvestmentSize.find_or_create_by(name: item) }
-
 [
   'SOLLA',
   'Later Life Academy',
   'ISO 22222',
   'British Standard in Financial Planning BS8577',
   'Other'
-].each { |item| Accreditation.find_or_create_by(name: item) }
+].each.with_index(1) { |item, index| Accreditation.find_or_create_by(name: item, order: index) }
 
 [
-  'Level 4 (DipPFS, DipFA® or equivalent)',
-  'Level 6 Diploma in Financial Advice (Adv DipFA®)',
-  'Chartered Financial Planner',
-  'Certified Financial Planner',
-  'Pension transfer qualifications - holder of G60, AF3 or equivalent',
-  'Equity release qualifications i.e. holder of Certificate in Equity Release or equivalent',
-  'Long term care planning qualifications i.e. holder of CF8 or equivalent'
-].each { |item| Qualification.find_or_create_by(name: item) }
+  'From their own resources',
+  'From funds to be invested'
+].each.with_index(1) { |item, index| AllowedPaymentMethod.find_or_create_by(name: item, order: index) }
+
+[
+  'At customers home',
+  'At firm\'s place of business',
+  'At an agreed location',
+].each.with_index(1) { |item, index| InPersonAdviceMethod.find_or_create_by(name: item, order: index) }
+
+[
+  'Hourly fee',
+  'Fixed fee',
+  'Other',
+].each.with_index(1) { |item, index| InitialAdviceFeeStructure.find_or_create_by(name: item, order: index) }
+
+[
+  'Hourly fee',
+  'Fixed fee',
+  'Other',
+].each.with_index(1) { |item, index| InitialAdviceFeeStructure.find_or_create_by(name: item, order: index) }
+
+[
+  'Under £50,000',
+  '£50,000 - £99,999',
+  '£100,000 - £149,999',
+  'Over £150,000'
+].each.with_index(1) { |item, index| InvestmentSize.find_or_create_by(name: item, order: index) }
+
+[
+  'Hourly fee',
+  'Fixed upfront fee',
+  'Monthly by direct debit / standing order',
+  'Other'
+].each.with_index(1) { |item, index| OngoingAdviceFeeStructure.find_or_create_by(name: item, order: index) }
+
+[
+  'Advice by telephone',
+  'Advice online (e.g. by video call / conference / email)'
+].each.with_index(1) { |item, index| OtherAdviceMethod.find_or_create_by(name: item, order: index) }
 
 [
   'Personal Finance Society / Chartered Insurance Institute',
@@ -67,7 +56,17 @@ FactoryGirl.create_list(:principal, 3) if Rails.env.development?
   'The Chartered Institute for Securities and Investments',
   'CFA Institute',
   'Institute of Chartered Accountants for England and Wales'
-].each do |item|
-  ProfessionalStanding.find_or_create_by(name: item)
-  ProfessionalBody.find_or_create_by(name: item)
+].each.with_index(1) do |item, index|
+  ProfessionalStanding.find_or_create_by(name: item, order: index)
+  ProfessionalBody.find_or_create_by(name: item, order: index)
 end
+
+[
+  'Level 4 (DipPFS, DipFA® or equivalent)',
+  'Level 6 Diploma in Financial Advice (Adv DipFA®)',
+  'Chartered Financial Planner',
+  'Certified Financial Planner',
+  'Pension transfer qualifications - holder of G60, AF3 or equivalent',
+  'Equity release qualifications i.e. holder of Certificate in Equity Release or equivalent',
+  'Long term care planning qualifications i.e. holder of CF8 or equivalent'
+].each.with_index(1) { |item, index| Qualification.find_or_create_by(name: item, order: index) }
