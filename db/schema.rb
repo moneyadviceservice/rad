bbec9f3a46318c2e73e724c099b32b96e30c48fc
Dan Munckton <dan@cultivatehq.com>
2015-04-24 16:28:34 +0100

    Update to mas-rad_core 0.0.46 which adds an index on lookup_advisers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305150719) do
+ActiveRecord::Schema.define(version: 20150423154732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -177,6 +177,8 @@ ActiveRecord::Schema.define(version: 20150305150719) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
+
+  add_index "lookup_advisers", ["reference_number"], name: "index_lookup_advisers_on_reference_number", unique: true, using: :btree
 
   create_table "lookup_firms", force: :cascade do |t|
     t.integer  "fca_number",                   null: false
