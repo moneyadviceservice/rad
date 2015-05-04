c444f1307aff09e4581ab87dc9e5d5f59f0447b8
Caden Lovelace <caden@herostrat.us>
2015-05-04 17:05:40 +0100

    Copy in frontend's sign in view.
    There are a number of things in there that need addressing. Layouts, for
    instance.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def devise_layout
+    content_tag :div, class: 'l-constrained' do
+      content_tag :div, class: 'l-registration' do
+        yield
+      end
+    end
+  end
 end
