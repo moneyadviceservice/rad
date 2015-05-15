13256a87ecd2f0fbc0dace68aa46ca5792d03ec1
Caden Lovelace <caden@herostrat.us>
2015-05-15 16:27:24 +0100

    [SQUASH] Use test classes rather than IDs.

--- spec/support/reset_password_page.rb
+++ spec/support/reset_password_page.rb
@@ -2,7 +2,7 @@ class ResetPasswordPage < SitePrism::Page
   set_url '/users/password/edit'
   set_url_matcher %r{/users/password/edit?reset_password_token=.*}
 
-  element :password_field, '#user_password'
-  element :password_confirmation_field, '#user_password_confirmation'
-  element :submit_button, '#new_user input[type="submit"]'
+  element :password_field, '.t-password-field'
+  element :password_confirmation_field, '.t-password-confirmation-field'
+  element :submit_button, '.t-submit-button'
 end
--- spec/support/sign_in_page.rb
+++ spec/support/sign_in_page.rb
@@ -2,9 +2,9 @@ class SignInPage < SitePrism::Page
   set_url '/users/sign_in'
   set_url_matcher %r{/users/sign_in}
 
-  element :email_field, '#user_email'
-  element :password_field, '#user_password'
-  element :submit_button, '#new_user input[type="submit"]'
+  element :email_field, '.t-email-field'
+  element :password_field, '.t-password-field'
+  element :submit_button, '.t-submit-button'
   element :forgot_password_link, '.t-forgot-password'
   element :flash_message, '.t-flash-message'
 end
