b59a30a3e89a13534c3b28cf1ac82ebd6ca31bf5
Caden Lovelace <caden@herostrat.us>
2015-05-15 16:37:46 +0100

    [SQUASH] More test classes.

--- spec/support/forgot_password_page.rb
+++ spec/support/forgot_password_page.rb
@@ -2,6 +2,6 @@ class ForgotPasswordPage < SitePrism::Page
   set_url '/users/password/new'
   set_url_matcher %r{/users/password/new}
 
-  element :email_field, '#user_email'
-  element :submit_button, '#new_user input[type="submit"]'
+  element :email_field, '.t-email-field'
+  element :submit_button, '.t-submit-button'
 end
