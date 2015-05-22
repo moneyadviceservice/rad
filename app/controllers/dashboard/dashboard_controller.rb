df26d3b24dd80a0cfc5f33a0ce8d6424c6de0172
Caden Lovelace <caden@herostrat.us>
2015-05-22 11:38:32 +0100

    Display firms and subsidiaries on dashboard page.

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -1,9 +1,20 @@
 module Dashboard
   class DashboardController < ApplicationController
     before_action :authenticate_user!
-    
+
     def index
+      @firms = all_firms_associated_with_principal
       render 'dashboard/index'
+    end
+
+    private
+
+    def principal
+      current_user.principal
+    end
+
+    def all_firms_associated_with_principal
+      [principal.firm].concat(principal.firm.subsidiaries)
     end
   end
 end
--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -3,6 +3,7 @@ RSpec.feature 'The principal dashboard' do
 
   scenario 'The principal can see a summary of the firms they are associated with' do
     given_i_am_a_fully_registered_principal_user
+    and_i_have_a_firm_with_subsidiaries
     and_i_am_logged_in
     when_i_am_on_the_principal_dashboard
     then_i_can_see_the_list_of_firms_i_am_associated_with
@@ -11,7 +12,10 @@ RSpec.feature 'The principal dashboard' do
   def given_i_am_a_fully_registered_principal_user
     @principal = FactoryGirl.create(:principal)
     @user = FactoryGirl.create(:user, principal: @principal)
-    firm_attrs = FactoryGirl.attributes_for(:firm, fca_number: @principal.fca_number)
+  end
+
+  def and_i_have_a_firm_with_subsidiaries
+    firm_attrs = FactoryGirl.attributes_for(:firm_with_subsidiaries, fca_number: @principal.fca_number)
     @principal.firm.update_attributes(firm_attrs)
   end
 
@@ -24,6 +28,9 @@ RSpec.feature 'The principal dashboard' do
   end
 
   def then_i_can_see_the_list_of_firms_i_am_associated_with
-    expect(dashboard_page.firms).to be
+    expect(dashboard_page.firms).to have_text @principal.firm.registered_name
+    @principal.firm.subsidiaries.each do |subsidiary|
+      expect(dashboard_page.firms).to have_text subsidiary.registered_name
+    end
   end
 end
