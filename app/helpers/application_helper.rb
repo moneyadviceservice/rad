ec836190ae7b21b6ba5b2df4de0ee4ae02eae04d
jiggypete <jiggypete@gmail.com>
2015-05-25 11:38:46 +0100

    Refactor devise layout helper into layout proper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def devise_layout
-    content_tag :div, class: 'l-constrained' do
-      content_tag :div, class: 'l-registration' do
-        yield
-      end
-    end
-  end
-
   def display_adviser_sign_in?
     current_user.nil?
   end
