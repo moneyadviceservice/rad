b0e412e3abb12f03e219d14d2e6520b48387be6c
Caden Lovelace <caden@herostrat.us>
2015-06-11 15:31:02 +0100

    Add create action to let Advisers be created.

--- app/controllers/dashboard/advisers_controller.rb
+++ app/controllers/dashboard/advisers_controller.rb
@@ -12,6 +12,20 @@ module Dashboard
       @adviser = advisers.build
     end
 
+    def create
+      @firm = Firm.find(params[:firm_id])
+      @adviser = advisers.build(adviser_params)
+      @adviser.confirmed_disclaimer = true
+      # ^^^ This needs to be resolved by some heavier refactoring
+      # work across mas-rad_core and this codebase.
+      if @adviser.save
+        flash[:notice] = I18n.t('dashboard.adviser_edit.saved')
+        render :edit
+      else
+        render :new
+      end
+    end
+
     private
 
     def principal
@@ -21,6 +35,18 @@ module Dashboard
     def advisers
       Firm.find_by(id: params[:firm_id], fca_number: principal.fca_number)
         .advisers
+    end
+
+    def adviser_params
+      params.require(:adviser).permit(
+        :reference_number,
+        :postcode,
+        :travel_distance,
+        accreditation_ids: [],
+        qualification_ids: [],
+        professional_standing_ids: [],
+        professional_body_ids: []
+      )
     end
   end
 end
