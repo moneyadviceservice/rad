a65afea39e765fc297d9b465e8a33868c88a9065
Caden Lovelace <caden@herostrat.us>
2015-06-15 16:02:10 +0100

    Refactor recently edited advisers helper

--- app/helpers/dashboard/dashboard_helper.rb
+++ app/helpers/dashboard/dashboard_helper.rb
@@ -24,31 +24,16 @@ module Dashboard
     end
 
     def number_of_advisers(firm)
-      all_advisers(firm).length
+      all_advisers_for_firm_and_trading_names(firm).size
     end
 
-    def most_recently_edited_advisers(firm)
-      return [] if firm.advisers.empty?
-
-      number_of_entries = 3
-      result = Array.new(number_of_entries)
-      advisers = all_advisers(firm).sort_by(&:updated_at).reverse.first(number_of_entries)
-
-      advisers.each_with_index do |adviser, index|
-        result[index] = adviser
-      end
-
-      result
+    def most_recently_edited_advisers(firm, limit = 3)
+      all_advisers_for_firm_and_trading_names(firm).order(updated_at: 'DESC').limit(limit)
     end
 
-    private
-
-    def all_advisers(firm)
-      result = firm.advisers
-      firm.trading_names.each do |trading_names|
-        result += trading_names.advisers
-      end
-      result
+    def all_advisers_for_firm_and_trading_names(firm)
+      firms = Firm.where(fca_number: firm.fca_number)
+      Adviser.where(firm: firms)
     end
   end
 end
--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -1,81 +1,59 @@
 RSpec.describe Dashboard::DashboardHelper, type: :helper do
   describe '#number_of_advisers' do
     it 'counts both parent and trading name advisers' do
-      first_trading_name = Firm.new advisers: [Adviser.new]
-      second_trading_name = Firm.new advisers: [Adviser.new, Adviser.new]
-
-      trading_names = [first_trading_name, second_trading_name]
-      firm = Firm.new(trading_names: trading_names, advisers: [Adviser.new])
+      firm = FactoryGirl.create(:firm_with_advisers, advisers_count: 1)
+      FactoryGirl.create(:firm_with_advisers, fca_number: firm.fca_number, advisers_count: 1)
+      FactoryGirl.create(:firm_with_advisers, fca_number: firm.fca_number, advisers_count: 2)
 
       expect(helper.number_of_advisers(firm)).to eq(4)
     end
   end
 
   describe '#most_recently_edited_advisers' do
-    it 'returns and empty list when there are no advisers' do
-      firm = Firm.new(advisers: [])
+    it 'returns an empty list when there are no advisers' do
+      firm = FactoryGirl.create(:firm)
       expect(helper.most_recently_edited_advisers(firm)).to eq([])
     end
 
-    it 'returns three advisers when there is one adviser' do
-      adviser = Adviser.new(reference_number: 'ABC12345')
-      firm = Firm.new(advisers: [adviser])
-
+    it 'returns one adviser when there is one adviser' do
+      firm = FactoryGirl.create(:firm_with_advisers, advisers_count: 1)
       most_recent = helper.most_recently_edited_advisers(firm)
 
-      expect(most_recent.length).to eq(3)
-      expect(most_recent[0]).to eq(adviser)
-      expect(most_recent[1]).to be_nil
-      expect(most_recent[2]).to be_nil
+      expect(most_recent.length).to eq(1)
+      expect(most_recent[0]).to eq(firm.advisers.first)
     end
 
-    it 'returns three advisers when there is two advisers' do
-      first_adviser = Adviser.new(reference_number: 'ABC12345', updated_at: 1.week.ago)
-      second_adviser = Adviser.new(reference_number: 'XYZ12345', updated_at: 2.weeks.ago)
-      advisers = [first_adviser, second_adviser]
-      firm = Firm.new(advisers: advisers)
+    it 'provides three advisers when there are four advisers' do
+      firm = FactoryGirl.create(:firm)
+      FactoryGirl.create_list(:adviser, 4, firm_id: firm.id)
 
       most_recent = helper.most_recently_edited_advisers(firm)
 
       expect(most_recent.length).to eq(3)
-      expect(most_recent[0]).to eq(first_adviser)
-      expect(most_recent[1]).to eq(second_adviser)
-      expect(most_recent[2]).to be_nil
     end
 
-    it 'returns three advisers when there is three advisers' do
-      first_adviser = Adviser.new(reference_number: 'ABC12345', updated_at: 1.week.ago)
-      second_adviser = Adviser.new(reference_number: 'XYZ12345', updated_at: 2.weeks.ago)
-      third_adviser = Adviser.new(reference_number: 'QWE12345', updated_at: 3.weeks.ago)
-      advisers = [first_adviser, second_adviser, third_adviser]
-      firm = Firm.new(advisers: advisers)
+    it 'provides the three most recently edited advisers when there are four advisers' do
+      firm = FactoryGirl.create(:firm)
+      first_adviser = FactoryGirl.create(:adviser, firm_id: firm.id, updated_at: 2.weeks.ago)
+      second_adviser = FactoryGirl.create(:adviser, firm_id: firm.id, updated_at: 1.weeks.ago)
+      fourth_adviser = FactoryGirl.create(:adviser, firm_id: firm.id, updated_at: 3.weeks.ago)
+      FactoryGirl.create(:adviser, firm_id: firm.id, updated_at: 4.weeks.ago)
 
       most_recent = helper.most_recently_edited_advisers(firm)
 
-      expect(most_recent.length).to eq(3)
-      expect(most_recent[0]).to eq(first_adviser)
-      expect(most_recent[1]).to eq(second_adviser)
-      expect(most_recent[2]).to eq(third_adviser)
+      expect(most_recent[0]).to eq(second_adviser)
+      expect(most_recent[1]).to eq(first_adviser)
+      expect(most_recent[2]).to eq(fourth_adviser)
     end
 
-    it 'provides the three most recently edited advisers when there are many advisers' do
-      first_adviser = Adviser.new(reference_number: 'AAA12345', updated_at: 1.week.ago)
-      second_adviser = Adviser.new(reference_number: 'BBB12345', updated_at: 2.weeks.ago)
-      third_adviser = Adviser.new(reference_number: 'CCC12345', updated_at: 3.weeks.ago)
-      fourth_adviser = Adviser.new(reference_number: 'DDD12345', updated_at: 4.weeks.ago)
-
-      first_trading_name = Firm.new advisers: [third_adviser]
-      second_trading_name = Firm.new advisers: [first_adviser]
-
-      trading_names = [first_trading_name, second_trading_name]
-      firm = Firm.new(trading_names: trading_names, advisers: [fourth_adviser, second_adviser])
+    it 'accounts for advisers on trading names of the firm' do
+      firm = FactoryGirl.create(:firm)
+      trading_name = FactoryGirl.create(:trading_name, fca_number: firm.fca_number)
+      FactoryGirl.create(:adviser, firm_id: firm.id)
+      FactoryGirl.create(:adviser, firm_id: trading_name.id)
 
       most_recent = helper.most_recently_edited_advisers(firm)
-
-      expect(most_recent.length).to eq(3)
-      expect(most_recent[0]).to eq(first_adviser)
-      expect(most_recent[1]).to eq(second_adviser)
-      expect(most_recent[2]).to eq(third_adviser)
+      expect(most_recent.length).to eq(2)
     end
   end
 end
