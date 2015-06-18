d60ffdf00de844729b6e79566465ec52fa8fe32f
Dan Munckton <dan@cultivatehq.com>
2015-06-18 12:53:36 +0100

    Refactor dashboard_spec in prep for making recently-edited lists

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -3,30 +3,40 @@ RSpec.feature 'The principal dashboard' do
 
   scenario 'The principal can see a summary of the firms they are associated with' do
     given_i_am_a_fully_registered_principal_user
-    and_i_have_a_firm_with_trading_names
     and_i_am_logged_in
+
     when_i_am_on_the_principal_dashboard
-    then_i_can_see_the_list_of_firms_i_am_associated_with
-    then_the_top_level_firm_is_first_and_labelled_main
-    and_the_trading_names_are_present_and_labelled_trading_names
+    then_i_can_see_the_list_of_recently_edited_firms(count: 1)
+    then_the_parent_firm_is_first_and_labelled_main
+
+    given_my_firm_has_trading_names(count: 2)
+    when_i_am_on_the_principal_dashboard
+    then_the_trading_names_are_present_and_labelled_trading_names
   end
 
   scenario 'The principal can see a summary of the advisers they are associated with' do
     given_i_am_a_fully_registered_principal_user
-    and_i_have_a_firm_with_trading_names
     and_i_am_logged_in
+
     when_i_am_on_the_principal_dashboard
     then_i_can_see_the_list_of_most_recently_edited_advisers
   end
 
   def given_i_am_a_fully_registered_principal_user
     @principal = FactoryGirl.create(:principal)
+    firm_attrs = FactoryGirl.attributes_for(:firm,
+                                            fca_number: @principal.fca_number,
+                                            registered_name: @principal.lookup_firm.registered_name)
+    @principal.firm.update_attributes(firm_attrs)
     @user = FactoryGirl.create(:user, principal: @principal)
   end
 
-  def and_i_have_a_firm_with_trading_names
-    firm_attrs = FactoryGirl.attributes_for(:firm_with_subsidiaries, fca_number: @principal.fca_number)
-    @principal.firm.update_attributes(firm_attrs)
+  def given_my_firm_has_trading_names(count:)
+    @principal.firm.trading_names = create_list(:trading_name,
+                                                count,
+                                                fca_number: @principal.fca_number)
+    @principal.firm.save!
+    expect(Firm.where(fca_number: @principal.fca_number).count).to eq(1 + count)
   end
 
   def and_i_am_logged_in
@@ -38,22 +48,22 @@ RSpec.feature 'The principal dashboard' do
     expect(dashboard_page).to be_displayed
   end
 
-  def then_i_can_see_the_list_of_firms_i_am_associated_with
-    expect(dashboard_page).to have_firms(count: @principal.firm.subsidiaries.count + 1)
+  def then_i_can_see_the_list_of_recently_edited_firms(count:)
+    expect(dashboard_page).to have_firms(count: count)
   end
 
-  def then_the_top_level_firm_is_first_and_labelled_main
+  def then_the_parent_firm_is_first_and_labelled_main
     expect(dashboard_page.firms.first).to have_name(text: @principal.firm.registered_name)
     expect(dashboard_page.firms.first).to have_type(text: 'Main')
   end
 
-  def and_the_trading_names_are_present_and_labelled_trading_names
-    names = @principal.firm.subsidiaries.map(&:registered_name)
+  def then_the_trading_names_are_present_and_labelled_trading_names
+    expect(dashboard_page).to have_trading_names(count: @principal.firm.trading_names.count)
+    expect(dashboard_page.trading_names).to rspec_all(have_type text: 'Trading name')
 
-    dashboard_page.trading_names.each do |trading_name|
-      expect(names).to include trading_name.name.text
-      expect(trading_name.type).to have_text 'Trading name'
-    end
+    expected_names = @principal.firm.trading_names.map(&:registered_name)
+    actual_names = dashboard_page.trading_names.map { |trading_name| trading_name.name.text }
+    expect(actual_names).to match_array(expected_names)
   end
 
   def then_i_can_see_the_list_of_most_recently_edited_advisers
