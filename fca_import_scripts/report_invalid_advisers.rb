8825ea9664ca78c25323ced6c73f56fc47c5d58a
Dan Munckton <dan@cultivatehq.com>
2015-07-06 15:59:12 +0100

    Convert the ext data search script into Ruby

--- fca_import_scripts/report_invalid_advisers.rb
+++ fca_import_scripts/report_invalid_advisers.rb
@@ -2,12 +2,50 @@
 require File.expand_path('../../config/environment',  __FILE__)
 require 'csv'
 
-def get_adviser_status(reference_number)
-  `./lib/adviser_status.sh #{reference_number}`.split(':').last.strip.upcase
+STATUS_STRINGS = {
+  '1' => 'Banned',
+  '2' => 'Applied',
+  '3' => 'Inactive',
+  '4' => 'Active',
+  '5' => 'Deceased'
+}.freeze
+FRN_INDEX = 0
+STATUS_INDEX = 4
+
+def log(message)
+  STDERR.puts(message)
 end
 
+def status_code_to_str(status_code)
+  STATUS_STRINGS.fetch(status_code, "UNKNOWN STATUS CODE (#{status_code})")
+end
+
+def load_adviser_statuses
+  log 'Loading adviser statuses'
+
+  frn_to_status_hash = File.open('ext/advisers-utf8.ext', 'r') do |file|
+    file.each_line.each_with_object({}) do |line, hash|
+      record = line.split('|')
+      next if %w(Header Footer).include? record[FRN_INDEX]
+      hash[record[FRN_INDEX]] = record[STATUS_INDEX]
+    end
+  end
+
+  frn_to_status_hash
+end
+
+def adviser_statuses
+  @adviser_statuses ||= load_adviser_statuses
+end
+
+def get_adviser_status(reference_number)
+  status_code_to_str(adviser_statuses[reference_number])
+end
+
+log 'Searching for invalid advisers'
 invalid_advisers = Adviser.all.select { |a| !a.valid? }
 
+log 'Processing list of invalid advisers'
 invalid_advisers.each do |adviser|
   if adviser.errors.full_messages.size == 1
     if adviser.errors.first[0] == :reference_number
@@ -27,3 +65,5 @@ invalid_advisers.each do |adviser|
     "http://radsignup.moneyadviceservice.org.uk/admin/advisers/#{adviser.id}"
   ].to_csv
 end
+
+log 'Done'
