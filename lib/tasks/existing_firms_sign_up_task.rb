9e853187d81d6bdeb2bfef47d3b8b97f00bcb381
jiggypete <jiggypete@gmail.com>
2015-07-15 15:44:23 +0100

    change rake task to produce easy to copy n paste csv list of principals to invite to rad

--- lib/tasks/existing_firms_sign_up_task.rb
+++ lib/tasks/existing_firms_sign_up_task.rb
@@ -1,22 +1,32 @@
 module Tasks
   class ExistingFirmsSignUpTask
-    def self.notify
+    def self.notify(inviter, output = STDOUT)
       valid_registered_parent_firms.each do |firm|
         principal = firm.principal
         user = User.find_by_principal_token principal.token
+        next if user.present?
 
-        if user.nil?
-          invite principal
-        elsif user.invited_to_sign_up? && !user.invitation_accepted?
-          user.invite!
-        end
+        user = invite principal
+        output << CSV.generate_line(build_csv_data(inviter, user, firm))
       end
+    end
+
+    def self.build_csv_data(inviter, user, firm)
+      result = []
+      result << firm.fca_number
+      result << firm.registered_name
+      result << user.principal.full_name
+      result << user.principal.email_address
+      result << inviter.invitation_url(user)
+      result
     end
 
     def self.invite(principal)
       User.invite!(
         principal_token: principal.token,
-        email: principal.email_address
+        email: principal.email_address,
+        skip_invitation: true,
+        invitation_sent_at: DateTime.current
       )
     end
 
--- lib/tasks/notify_existing_firms_without_accounts.rake
+++
@@ -1,7 +0,0 @@
-
-namespace :notify do
-  desc 'existing firms need notified to sign up'
-  task existing_firms_to_sign_up: :environment do
-    Tasks::ExistingFirmsSignUpTask.notify
-  end
-end
