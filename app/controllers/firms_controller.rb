3a5e58e585b63e986616d5c707ca2dd4c562f06c
Caden Lovelace <caden@herostrat.us>
2015-08-14 15:31:57 +0100

    Remove unused controllers, views, tests, from removed signup flow

--- app/controllers/firms_controller.rb
+++
@@ -1,9 +0,0 @@
-class FirmsController < PrincipalsBaseController
-  def index
-    if current_principle.subsidiaries?
-      @firm = current_principle.lookup_firm
-    else
-      redirect_to edit_principal_firm_questionnaire_path(current_principle, current_principle.firm)
-    end
-  end
-end
