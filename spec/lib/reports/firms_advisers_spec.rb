ee53707f51e920ced10b357040175cc4fc23e56e
Alan Gardner <urfolomeus@gmail.com>
2016-02-18 17:41:27 +0000

    A small refactoring

--- spec/lib/reports/firms_advisers_spec.rb
+++ spec/lib/reports/firms_advisers_spec.rb
@@ -1,24 +1,17 @@
 RSpec.describe Reports::FirmsAdvisers do
   describe '#data' do
-    subject(:data_with_header) { CSV.parse(Reports::FirmsAdvisers.data) }
-    subject(:data_without_header) do
-      data = data_with_header
-      data.shift
-      data
-    end
+    let(:firm_count) { 3 }
+    let!(:firms)     { FactoryGirl.create_list(:principal, firm_count).map(&:firm) }
 
-    before :each do
-      @firm_count = 3
-
-      @firms = FactoryGirl.create_list(:principal, @firm_count).map(&:firm)
-    end
+    subject(:data_with_header)    { CSV.parse(Reports::FirmsAdvisers.data) }
+    subject(:data_without_header) { data_with_header[1..-1] }
 
     it 'has a header row' do
       expect(data_with_header.first).to eq(Reports::FirmsAdvisers::HEADER_ROW)
     end
 
     it 'has a row for each firm' do
-      expect(data_without_header.size).to eq(@firm_count)
+      expect(data_without_header.size).to eq(firm_count)
     end
 
     describe 'each firm row' do
@@ -30,28 +23,28 @@ RSpec.describe Reports::FirmsAdvisers do
       COL_ADVISER_COUNT = 5
 
       it 'has an FCA Number' do
-        @firms.each_with_index do |firm, i|
+        firms.each_with_index do |firm, i|
           expect(firm.fca_number).not_to be_nil
           expect(data_without_header[i][COL_FCA_NUMBER]).to eq(firm.fca_number.to_s)
         end
       end
 
       it 'has a Name' do
-        @firms.each_with_index do |firm, i|
+        firms.each_with_index do |firm, i|
           expect(firm.registered_name).not_to be_nil
           expect(data_without_header[i][COL_NAME]).to eq(firm.registered_name)
         end
       end
 
       it 'has a Principal' do
-        @firms.each_with_index do |firm, i|
+        firms.each_with_index do |firm, i|
           expect(firm.principal.full_name).not_to be_nil
           expect(data_without_header[i][COL_PRINCIPAL]).to eq(firm.principal.full_name)
         end
       end
 
       it 'has an Email Address' do
-        @firms.each_with_index do |firm, i|
+        firms.each_with_index do |firm, i|
           expect(firm.principal.email_address).not_to be_nil
           expect(data_without_header[i][COL_EMAIL]).to eq(firm.principal.email_address)
         end
@@ -60,9 +53,9 @@ RSpec.describe Reports::FirmsAdvisers do
 
     describe 'advice method' do
       before :each do
-        @firms[0].in_person_advice_methods.create(FactoryGirl.attributes_for(:in_person_advice_method))
-        @firms[1].in_person_advice_methods.create(FactoryGirl.attributes_for(:in_person_advice_method))
-        @firms[2].other_advice_methods.create(FactoryGirl.attributes_for(:other_advice_method))
+        firms[0].in_person_advice_methods.create(FactoryGirl.attributes_for(:in_person_advice_method))
+        firms[1].in_person_advice_methods.create(FactoryGirl.attributes_for(:in_person_advice_method))
+        firms[2].other_advice_methods.create(FactoryGirl.attributes_for(:other_advice_method))
       end
 
       it 'shows local if the firm is local' do
@@ -77,9 +70,9 @@ RSpec.describe Reports::FirmsAdvisers do
 
     describe 'adviser count' do
       before :each do
-        FactoryGirl.create(:adviser, firm: @firms[1])
-        FactoryGirl.create(:adviser, firm: @firms[1])
-        FactoryGirl.create(:adviser, firm: @firms[2])
+        FactoryGirl.create(:adviser, firm: firms[1])
+        FactoryGirl.create(:adviser, firm: firms[1])
+        FactoryGirl.create(:adviser, firm: firms[2])
       end
 
       it 'shows 0 if a firm has no advisers' do
