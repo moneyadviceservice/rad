1eaa7164ff313a6695ac18e44e2854bdfb53ce2a
Alan Gardner <urfolomeus@gmail.com>
2016-02-23 11:55:45 +0000

    Adding firm details status

--- app/presenters/self_service/status_presenter.rb
+++ app/presenters/self_service/status_presenter.rb
@@ -1,5 +1,8 @@
 module SelfService
   class StatusPresenter
+    include Rails.application.routes.url_helpers
+    include ActionView::Helpers::UrlHelper
+
     def initialize(firm)
       @firm = firm
     end
@@ -10,6 +13,21 @@ module SelfService
 
     def overall_status_icon
       @firm.publishable? ? 'tick' : 'exclamation'
+    end
+
+    def firm_details_icon
+      @firm.registered? ? 'tick' : 'exclamation'
+    end
+
+    def firm_details_link(opts = {})
+      link_to 'Edit', firm_details_link_path, opts
+    end
+
+    private
+
+    def firm_details_link_path
+      route = @firm.trading_name? ? 'trading_name' : 'firm'
+      send "edit_self_service_#{route}_path", @firm
     end
   end
 end
--- spec/presenters/self_service/status_presenter_spec.rb
+++ spec/presenters/self_service/status_presenter_spec.rb
@@ -1,9 +1,13 @@
 RSpec.describe SelfService::StatusPresenter do
+  def link_regex_for(route, text)
+    /^<a.*href=.*#{route}.*#{firm.id}.*edit.*>#{text}<\/a>/
+  end
+
+  let(:firm) { FactoryGirl.create(:firm) }
+
   subject(:presenter) { described_class.new(firm) }
 
   context 'when the firm is publishable' do
-    let(:firm) { FactoryGirl.create(:firm) }
-
     describe '#overall_status' do
       it 'provides "published" ' do
         expect(presenter.overall_status).to eq('published')
@@ -11,7 +15,7 @@ RSpec.describe SelfService::StatusPresenter do
     end
 
     describe '#overall_status_icon' do
-      it 'provides "tick" ' do
+      it 'provides "tick"' do
         expect(presenter.overall_status_icon).to eq('tick')
       end
     end
@@ -27,8 +31,48 @@ RSpec.describe SelfService::StatusPresenter do
     end
 
     describe '#overall_status_icon' do
-      it 'provides "exclamation" ' do
+      it 'provides "exclamation"' do
         expect(presenter.overall_status_icon).to eq('exclamation')
+      end
+    end
+  end
+
+  describe '#firm_details_icon' do
+    before { allow(firm).to receive(:registered?).and_return(firm_registered) }
+
+    context 'when the firm is registered' do
+      let(:firm_registered) { true }
+
+      it 'provides "tick"' do
+        expect(presenter.firm_details_icon).to eq('tick')
+      end
+    end
+
+    context 'when the firm is not registered' do
+      let(:firm_registered) { false }
+
+      it 'provides "exclamation"' do
+        expect(presenter.firm_details_icon).to eq('exclamation')
+      end
+    end
+  end
+
+  describe '#firm_details_link' do
+    before { allow(firm).to receive(:trading_name?).and_return(is_trading_name) }
+
+    context 'when the firm is a trading name' do
+      let(:is_trading_name) { true }
+
+      it 'provides a link to the trading name edit page' do
+        expect(presenter.firm_details_link).to match(link_regex_for('trading_names', 'Edit'))
+      end
+    end
+
+    context 'when the firm is not a trading name' do
+      let(:is_trading_name) { false }
+
+      it 'provides a link to the parent firm edit page' do
+        expect(presenter.firm_details_link).to match(link_regex_for('firms', 'Edit'))
       end
     end
   end
