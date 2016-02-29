ee92be13ce6cf21109581a371e09ba2c318b8b73
Dan Munckton <dan@cultivatehq.com>
2016-02-29 11:33:24 +0000

    Revert "Adding Mailjet"
    This reverts commit 6230b1b67a7ff90c4fab640f2a78c77e91126b58.

--- config/initializers/mailjet.rb
+++
@@ -1,7 +0,0 @@
-if Rails.env.production?
-  Mailjet.configure do |config|
-    config.api_key = ENV['MAILJET_API_KEY']
-    config.secret_key = ENV['MAILJET_SECRET_KEY']
-    config.default_from = 'RADenquiries@moneyadviceservice.org.uk'
-  end
-end
