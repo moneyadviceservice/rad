9762c50382cca25f5d7a9423243df929292b37a8
Mark Connell <mark@markconnell.co.uk>
2016-03-09 15:58:15 +0000

    Don't include advisers which have bypassed the reference checking process

--- app/controllers/admin/reports/inactive_advisers_controller.rb
+++ app/controllers/admin/reports/inactive_advisers_controller.rb
@@ -5,7 +5,8 @@ module Admin
         @inactive_advisers = Adviser
                              .joins('LEFT JOIN lookup_advisers ' \
                                    'ON lookup_advisers.reference_number = advisers.reference_number')
-                             .where('lookup_advisers.id' => nil)
+                             .where('lookup_advisers.id' => nil,
+                                    'advisers.bypass_reference_number_check' => false)
       end
     end
   end
