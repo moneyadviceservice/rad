af56b0d519188584df9407751157f85fc292fb50
Dan Munckton <dan@cultivatehq.com>
2016-03-09 11:54:39 +0000

    Ensure new advisers without reference numbers are geocoded when created

--- spec/features/admin/new_adviser_spec.rb
+++ spec/features/admin/new_adviser_spec.rb
@@ -1,4 +1,5 @@
-RSpec.feature 'Add a new adviser without an FCA number' do
+RSpec.feature 'Add a new adviser without an FCA number',
+              vcr: vcr_options_for(:features_admin_advisers_add_spec) do
   let(:firm_page) { Admin::FirmPage.new }
   let(:new_adviser_page) { Admin::NewAdviserPage.new }
 
@@ -17,6 +18,7 @@ RSpec.feature 'Add a new adviser without an FCA number' do
 
     then_i_am_on_the_firm_page
     and_i_can_see_1_adviser
+    and_they_have_been_geocoded
   end
 
   scenario 'Adding an adviser after we fail once' do
@@ -85,6 +87,10 @@ RSpec.feature 'Add a new adviser without an FCA number' do
 
   def and_i_can_see_1_adviser
     expect(firm_page.advisers.count).to eq(1)
+  end
+
+  def and_they_have_been_geocoded
+    expect(@firm.advisers.first).to be_geocoded
   end
 
   def then_no_errors_are_displayed_on(the_page:)
