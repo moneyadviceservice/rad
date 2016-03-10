28fc47b6343601ad4f10a8a3abd6dd83f50837cc
Dan Munckton <dan@cultivatehq.com>
2016-03-10 15:16:34 +0000

    Implement CSV download action for inactive firms report

--- spec/controllers/admin/reports/inactive_firms_controller_spec.rb
+++ spec/controllers/admin/reports/inactive_firms_controller_spec.rb
@@ -15,5 +15,31 @@ RSpec.describe Admin::Reports::InactiveFirmsController, type: :controller do
       get :show
       expect(assigns[:inactive_firms]).to eq([inactive_firm])
     end
+
+    context 'CSV format' do
+      render_views
+
+      it 'renders the csv template' do
+        get :show, format: :csv
+        expect(response).to render_template(:show)
+      end
+
+      it 'sets the content type to "text/csv"' do
+        get :show, format: :csv
+        expect(response.content_type).to eq('text/csv')
+      end
+
+      it 'has the appropriate csv headers' do
+        get :show, format: :csv
+        expect(response.body).to include('FCA Number,Registered Name,Visible on Directory?')
+      end
+
+      it 'renders inactive adviser content' do
+        FactoryGirl.create(:firm, fca_number: 789012, registered_name: 'Acme Finance')
+
+        get :show, format: :csv
+        expect(response.body).to include('789012,Acme Finance,Yes')
+      end
+    end
   end
 end
