fbf0075f3728b42e5491108dd2cb08fdf3082bce
Jon Gilbraith <jon@hobocode.io>
2016-07-11 15:37:21 +0100

    Fix scoping issues with some of the features

--- spec/features/admin/delete_adviser_spec.rb
+++ spec/features/admin/delete_adviser_spec.rb
@@ -8,24 +8,24 @@ RSpec.feature 'Deleting an adviser from the admin interface' do
     then_the_adviser_is_deleted
     and_i_am_redirected_to_the_adviser_list_page
   end
-end
 
-def given_there_is_an_adviser
-  @adviser = create(:adviser)
-end
+  def given_there_is_an_adviser
+    @adviser = create(:adviser)
+  end
 
-def when_i_visit_an_adviser_page
-  visit(admin_adviser_path(@adviser))
-end
+  def when_i_visit_an_adviser_page
+    visit(admin_adviser_path(@adviser))
+  end
 
-def and_i_click_delete_adviser
-  admin_adviser_page.delete_adviser.click
-end
+  def and_i_click_delete_adviser
+    admin_adviser_page.delete_adviser.click
+  end
 
-def then_the_adviser_is_deleted
-  expect(Adviser.find_by_id(@adviser.id)).to be_nil
-end
+  def then_the_adviser_is_deleted
+    expect(Adviser.find_by_id(@adviser.id)).to be_nil
+  end
 
-def and_i_am_redirected_to_the_adviser_list_page
-  expect(page.current_path).to eq admin_advisers_path
+  def and_i_am_redirected_to_the_adviser_list_page
+    expect(page.current_path).to eq admin_advisers_path
+  end
 end
--- spec/features/admin/delete_principal_and_entire_firm_spec.rb
+++ spec/features/admin/delete_principal_and_entire_firm_spec.rb
@@ -8,44 +8,44 @@ RSpec.feature 'Deleting principal and all related firm, adviser, office and trad
     then_the_principal_and_all_related_data_is_removed
     and_i_am_redirected_to_the_principal_list_page
   end
-end
 
-def given_there_is_firm
-  @firm = FactoryGirl.create(:firm_with_trading_names, :with_principal)
-  @principal = @firm.principal
-  @principal.update(firm: @firm)
-  @user = FactoryGirl.create(:user, principal: @principal)
+  def given_there_is_firm
+    @firm = FactoryGirl.create(:firm_with_trading_names, :with_principal)
+    @principal = @firm.principal
+    @principal.update(firm: @firm)
+    @user = FactoryGirl.create(:user, principal: @principal)
 
-  expect(User.find_by(principal: @principal)).to eq(@user)
-  expect(@principal).not_to be_nil
-  expect(@firm.offices.count).to be(1)
-  expect(@firm.advisers.count).to be(1)
-  expect(@firm.trading_names.count).to be(3)
+    expect(User.find_by(principal: @principal)).to eq(@user)
+    expect(@principal).not_to be_nil
+    expect(@firm.offices.count).to be(1)
+    expect(@firm.advisers.count).to be(1)
+    expect(@firm.trading_names.count).to be(3)
 
-  @office = @firm.offices[0]
-  @adviser = @firm.advisers[0]
-  @trading_name = @firm.trading_names[0]
-end
+    @office = @firm.offices[0]
+    @adviser = @firm.advisers[0]
+    @trading_name = @firm.trading_names[0]
+  end
 
-def when_i_visit_the_principal_page
-  admin_principal_page.load(principal_token: @principal.id)
-  expect(admin_principal_page).to be_displayed
-end
+  def when_i_visit_the_principal_page
+    admin_principal_page.load(principal_token: @principal.id)
+    expect(admin_principal_page).to be_displayed
+  end
 
-def and_i_click_delete
-  admin_principal_page.delete.click
-end
+  def and_i_click_delete
+    admin_principal_page.delete.click
+  end
 
-def then_the_principal_and_all_related_data_is_removed
-  expect(Principal.exists?(@principal.id)).to be(false)
-  expect(Firm.exists?(@firm.id)).to be(false)
-  expect(User.exists?(@user.id)).to be(false)
-  expect(Office.exists?(@office.id)).to be(false)
-  expect(Adviser.exists?(@adviser.id)).to be(false)
-  expect(Firm.exists?(@trading_name.id)).to be(false)
-end
+  def then_the_principal_and_all_related_data_is_removed
+    expect(Principal.exists?(@principal.id)).to be(false)
+    expect(Firm.exists?(@firm.id)).to be(false)
+    expect(User.exists?(@user.id)).to be(false)
+    expect(Office.exists?(@office.id)).to be(false)
+    expect(Adviser.exists?(@adviser.id)).to be(false)
+    expect(Firm.exists?(@trading_name.id)).to be(false)
+  end
 
-def and_i_am_redirected_to_the_principal_list_page
-  expect(page.current_path).to eq admin_principals_path
-  expect(find('.t-flash-message')).to have_text('Successfully deleted')
+  def and_i_am_redirected_to_the_principal_list_page
+    expect(page.current_path).to eq admin_principals_path
+    expect(find('.t-flash-message')).to have_text('Successfully deleted')
+  end
 end
