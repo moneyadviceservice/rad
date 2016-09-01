15e31dca4f630e48c3f9280a251eb68750b2f7dc
Jerome Riga <jerome.riga@moneyadviceservice.org.uk>
2016-09-01 12:07:21 +0100

    Updated travis config

--- app/jobs/fca_import_job.rb
+++ app/jobs/fca_import_job.rb
@@ -1,8 +1,12 @@
+require 'digest/sha1'
+
 class FcaImportJob < ActiveJob::Base
   include Sidekiq::Worker
-
+  sidekiq_options unique:      :until_executed,
+                  unique_args: ->(args) { Digest::SHA1.hexdigest(args.first.to_s) }
   queue_as :default
 
-  def perform(_files_to_process)
+  def perform(files, users)
+    FCA::Import.call(files) { |outcome, log| MAS::Notify.call(users, outcome, log) }
   end
 end
