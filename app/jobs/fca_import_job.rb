5b22d61f8aaabebec4f300f2de696ca8a1624ac1
Jerome Riga <jerome.riga@moneyadviceservice.org.uk>
2016-09-27 13:28:00 +0100

    Added fca import mailer

--- app/jobs/fca_import_job.rb
+++ app/jobs/fca_import_job.rb
@@ -43,34 +43,46 @@ class FcaImportJob < ActiveJob::Base
     @slack ||= Slack::Web::Client.new
   end
 
-  def slack_formatter(outcomes)
-    text = ''
-    if import_successful?(outcomes)
-      text = "<!here> The FCA data have been loaded into RAD. Visit #{admin_lookup_fca_import_index_url} to confirm that the data looks ok" # rubocop:disable all
-    else
-      erroneous = outcomes
-                  .select { |(_, s, _)| s == false }
-                  .map { |(f, _, o)| "Zip file #{f} caused error: #{o}" }
-      erroneous.insert(0, '<!here> Import has failed')
-      erroneous.insert(-1, "You can cancel this import here #{admin_lookup_fca_import_index_url}")
-      text = erroneous.join("\n")
-    end
-
-    logger.info("Notification msg: #{text}")
-    {
-      channel: FCA::Config.notify[:slack][:channel],
-      as_user: true,
-      text:    text
-    }
-  end
-
   def default_url_options
     { host: FCA::Config.hostname }
   end
 
-  def notify(outcomes)
-    slack.chat_postMessage(slack_formatter(outcomes))
+  def notify_slack(text)
+    tries ||= 1
+    slack.chat_postMessage(
+      channel: FCA::Config.notify[:slack][:channel],
+      as_user: true,
+      text:    "<!here> #{text}"
+    )
   rescue
     logger.error 'An error occured while trying to post msg'
+    retry if (tries += 1) <= 3
+  end
+
+  def notify_email(text)
+    FcaImportMailer.notify(
+      FCA::Config.email_recipients,
+      text
+    ).deliver_later
+  end
+
+  def notification_text(outcomes)
+    if import_successful?(outcomes)
+      "The FCA data have been loaded into RAD. Visit #{admin_lookup_fca_import_index_url} to confirm that the data looks ok" # rubocop:disable all
+    else
+      erroneous = outcomes
+                  .select { |(_, s, _)| s == false }
+                  .map { |(f, _, o)| "Zip file #{f} caused error: #{o}" }
+      erroneous.insert(0, 'Import has failed')
+      erroneous.insert(-1, "You can cancel this import here #{admin_lookup_fca_import_index_url}")
+      erroneous.join("\n")
+    end
+  end
+
+  def notify(outcomes)
+    text = notification_text(outcomes)
+    logger.info("Notification msg: #{text}")
+    notify_email(text)
+    notify_slack(text)
   end
 end
