dc0c75e66cc72afadde580e226bb550bccda8fac
Tomas D'Stefano <tomas_stefano@successoft.com>
2019-03-25 15:28:48 +0000

    Make sure geocoding has your own cassettes and cassettes dir

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,6 +2,27 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+
+  config.filter_sensitive_data('<API_KEY>') { ENV['GOOGLE_GEOCODER_API_KEY'] }
+
+  config.around_http_request do |request|
+    uri = URI(request.uri)
+
+    if uri.host =~ /google/
+      filtered_query = CGI.parse(uri.query).except('key').to_query
+      cassette_name = "/google/#{uri.path}/#{filtered_query}"
+      VCR.use_cassette(
+        cassette_name,
+        match_requests_on: [
+          :uri,
+          VCR.request_matchers.uri_without_param(:key)
+        ],
+        &request
+      )
+    else
+      request.proceed
+    end
+  end
 end
 
 def vcr_options_for(cassette_name)
