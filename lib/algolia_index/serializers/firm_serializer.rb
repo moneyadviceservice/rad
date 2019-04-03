48f25d0d5b6e37b7a248c711c03c35ffed907fb3
Giuseppe Lobraico <Giuseppe.Lobraico@MoneyAdviceService.org.uk>
2019-04-03 16:50:05 +0100

    Do not rename firm attributes ending with _flag
    Any real reason to rename them?

--- lib/algolia_index/serializers/firm_serializer.rb
+++ lib/algolia_index/serializers/firm_serializer.rb
@@ -8,12 +8,12 @@ module AlgoliaIndex
                :email_address,
                :free_initial_meeting,
                :minimum_fixed_fee,
-               :retirement_income_products,
-               :pension_transfer,
-               :options_when_paying_for_care,
-               :equity_release,
-               :inheritance_tax_planning,
-               :wills_and_probate,
+               :retirement_income_products_flag,
+               :pension_transfer_flag,
+               :long_term_care_flag,
+               :equity_release_flag,
+               :inheritance_tax_and_estate_planning_flag,
+               :wills_and_probate_flag,
                :other_advice_methods,
                :investment_sizes,
                :in_person_advice_methods,
@@ -61,30 +61,6 @@ module AlgoliaIndex
 
     def postcode_searchable
       object.postcode_searchable?
-    end
-
-    def retirement_income_products
-      object.retirement_income_products_flag
-    end
-
-    def pension_transfer
-      object.pension_transfer_flag
-    end
-
-    def options_when_paying_for_care
-      object.long_term_care_flag
-    end
-
-    def equity_release
-      object.equity_release_flag
-    end
-
-    def inheritance_tax_planning
-      object.inheritance_tax_and_estate_planning_flag
-    end
-
-    def wills_and_probate
-      object.wills_and_probate_flag
     end
 
     def other_advice_methods
