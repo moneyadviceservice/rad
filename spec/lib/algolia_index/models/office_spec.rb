290439c26dbd46f4b4984838e6244db2bd2f60a0
Giuseppe Lobraico <Giuseppe.Lobraico@MoneyAdviceService.org.uk>
2019-04-16 16:36:57 +0100

    Fix bug when creating offices index

--- spec/lib/algolia_index/models/office_spec.rb
+++ spec/lib/algolia_index/models/office_spec.rb
@@ -13,6 +13,27 @@ RSpec.describe AlgoliaIndex::Office do
 
   it { expect(described_class < AlgoliaIndex::Base).to eq(true) }
 
+  describe '.create!' do
+    let!(:offices) { FactoryGirl.create_list(:office, 3, firm_id: 1) }
+    let(:serialized) do
+      offices.map do |office|
+        AlgoliaIndex::OfficeSerializer.new(office)
+      end
+    end
+
+    before do
+      allow(AlgoliaIndex::OfficeSerializer).to receive(:new)
+        .and_return(*serialized)
+    end
+
+    it 'replaces offices in the index' do
+      expect(index_offices).to receive(:replace_all_objects)
+        .with(serialized).exactly(:once)
+
+      described_class.create!(offices)
+    end
+  end
+
   describe '#update!' do
     let!(:office) { FactoryGirl.create(:office, id: id, firm_id: 1) }
     let(:serialized) { AlgoliaIndex::OfficeSerializer.new(office) }
