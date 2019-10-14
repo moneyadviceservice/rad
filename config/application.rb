d101ec9ab99c30d304de05007b6f742b1b994e19
Malcolm Baig <malcolm.baig@gmail.com>
2019-10-14 14:32:07 +0100

    Switch off sassc concurrency
    Rails 5 uses sass-rails 6.0.0 (up from 4.0.5 which is what we were
    installing pre-upgrade). This has a dependency on sassc-rails, which
    appears to have some buggy concurrency behaviour at present.
    
    Disable concurrency and provide a link to the relevant issue.

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,23 @@ module Rad
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
 
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[
+      Rails.root.join('config', 'locales', '**', '*.{rb,yml}')
+    ]
 
     config.action_mailer.default_options = {
       from: 'RADenquiries@moneyadviceservice.org.uk',
       to: 'RADenquiries@moneyadviceservice.org.uk'
     }
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Settings in config/environments/* take precedence over those specified
+    # here.  Application configuration should go into files in
+    # config/initializers -- all .rb files in that directory are automatically
+    # loaded.
+
+    # Switch off sassc concurrency. See this issue
+    # https://github.com/rails/sprockets/issues/581#issuecomment-486984663
+    config.assets.configure do |env|
+      env.export_concurrent = false
+    end
   end
 end
