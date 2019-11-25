794da55d83adb2b0e8e66e1ed80550c7c6870a32
Tomas D'Stefano <tomas_stefano@successoft.com>
2019-11-25 14:29:24 +0000

    Add cohesiveness to admin retirement and travel insurance firms
    Now both retirement controller and travel insurance will have
    a base firms controller where the same behaviour is held

--- app/controllers/admin/travel_insurance_firms_controller.rb
+++ app/controllers/admin/travel_insurance_firms_controller.rb
@@ -1,10 +1,12 @@
-class Admin::TravelInsuranceFirmsController < Admin::FirmsController
-  def index
-    @search = TravelInsuranceFirm.ransack(params[:q])
-    @firms = @search.result.page(params[:page]).per(20)
+class Admin::TravelInsuranceFirmsController < Admin::BaseFirmsController
+  def firms_search_path
+    admin_travel_insurance_firms_path
   end
+  helper_method :firms_search_path
 
-  def show
-    @firm = TravelInsuranceFirm.find(params[:id])
+  private
+
+  def resource_class
+    TravelInsuranceFirm
   end
 end
