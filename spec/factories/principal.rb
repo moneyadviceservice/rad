188c687f377d65f3de9f4c20e373e502ec9d12a9
Malcolm Baig <malcolm.baig@gmail.com>
2019-11-28 10:29:18 +0000

    Fix principal factory
    Change how the associated default firm is constructed. This fixes a
    problem where passing a firm into the factory explicitly would result
    in two firms being built.

--- spec/factories/principal.rb
+++ spec/factories/principal.rb
@@ -8,19 +8,12 @@ FactoryBot.define do
     telephone_number { '07111 333 222' }
     confirmed_disclaimer { true }
 
-    # TODO: this is problematic if you manually construct your own firm in a
-    # spec using FactoryBot and associate it with the principal. You end up
-    # with two firms in the database, which is easy to miss and can lead to
-    # unexpected bugs. Revisit this and find a better way to associate an
-    # invalid Firm (which is a legitimate state for the system to be in, ie - a
-    # newly registered principal is expected to complete certain details
-    # post-signup).
     after(:build) do |principal|
-      Firm.new(
-        fca_number: principal.fca_number,
-        registered_name:  Faker::Name.first_name
-      ).tap do |f|
-        f.save!(validate: false)
+      if principal.firm.blank?
+        principal.firm = Firm.new(
+          fca_number: principal.fca_number,
+          registered_name:  Faker::Name.first_name
+        )
       end
     end
   end
