1bf8449cdc2aed9b9b1e6986028a635d26fb4408
Tomas D'Stefano <tomas_stefano@successoft.com>
2019-12-11 11:05:09 +0000

    Separate views from travel principal and retirement principal

--- spec/support/admin/edit_principal_user_page.rb
+++ spec/support/admin/edit_principal_user_page.rb
@@ -1,6 +1,6 @@
 class Admin::EditPrincipalUserPage < SitePrism::Page
-  set_url '/admin/principals/{principal_token}/user/edit'
-  set_url_matcher %r{/admin/principals/[a-z0-9]+/user/edit}
+  set_url '/admin/retirement_principals/{principal_token}/user/edit'
+  set_url_matcher %r{/admin/retirement_principals/[a-z0-9]+/user/edit}
 
   element :email_address, '.t-email-address'
   element :password, '.t-password'
--- spec/support/admin/principal_page.rb
+++ spec/support/admin/principal_page.rb
@@ -1,6 +1,6 @@
 class Admin::PrincipalPage < SitePrism::Page
-  set_url '/admin/principals/{principal_token}'
-  set_url_matcher %r{/admin/principals/[a-z0-9]+}
+  set_url '/admin/retirement_principals/{principal_token}'
+  set_url_matcher %r{/admin/retirement_principals/[a-z0-9]+}
 
   element :edit_directory_information, '.t-edit-principal'
   element :edit_login_credentials, '.t-edit-user'
