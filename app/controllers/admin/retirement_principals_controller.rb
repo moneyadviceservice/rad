66a882fbd1d3b14f7105c1d04c1fb9353897b519
Tomas D'Stefano <tomas_stefano@successoft.com>
2019-12-12 17:58:33 +0000

    Add features test to principals tab

--- app/controllers/admin/retirement_principals_controller.rb
+++ app/controllers/admin/retirement_principals_controller.rb
@@ -2,4 +2,9 @@ class Admin::RetirementPrincipalsController < Admin::PrincipalsController
   def collection
     Principal.joins(:firm)
   end
+
+  def collection_path
+    admin_retirement_principals_path
+  end
+  helper_method :collection_path
 end
