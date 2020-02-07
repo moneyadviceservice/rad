051d77ce00623249257390e26c29cc24813d6b47
Abraham Kiggundu <abraham@thekiggys.com>
2020-02-07 09:32:19 +0000

    [11202] converted property to helper method so email is easier to override

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :admin_email_address
 
   def after_sign_in_path_for(user)
     stored_location_for(user) || self_service_root_path
@@ -19,6 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def admin_email_address
-    @admin_email_address = ActionMailer::Base.default[:from]
+    ActionMailer::Base.default[:from]
   end
+  helper_method :admin_email_address
 end
