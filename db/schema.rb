5cbe7f4e8c4b5a5cf576e08cfa8cef8dde1dbb6e
Abraham Kiggundu <abraham@thekiggys.com>
2020-04-29 15:51:35 +0100

    [TP-11365] renamed travel migrations to future so as not to confuse things. Should change 3 to 2 when ready to merge travel directory release feature branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_133118) do
+ActiveRecord::Schema.define(version: 3019_11_18_100139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -400,4 +400,5 @@ ActiveRecord::Schema.define(version: 2020_01_29_133118) do
     t.index ["password_changed_at"], name: "index_users_on_password_changed_at"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
