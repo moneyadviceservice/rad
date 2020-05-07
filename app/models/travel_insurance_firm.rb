8f0f37f13585df1300f83e8a95e718270b82d31d
Abraham Kiggundu <abraham@thekiggys.com>
2020-05-07 21:56:44 +0100

    [TP-11365] various model fixes to get stuff working and tests passing

--- app/models/travel_insurance_firm.rb
+++ app/models/travel_insurance_firm.rb
@@ -22,34 +22,32 @@ class TravelInsuranceFirm < ApplicationRecord
     sub_arachnoid_haemorrhage_and_epilepsy_question
   ].freeze
 
-  # TODO: these might get stale if the fca lookup fails so implement a regular clean of the cache or consider using a gem that does so instead of re-inventing the cache implementation wheel
-  REGISTRATION_QUESTION_ANSWERS = Hash.new({})
-
   belongs_to :principal, primary_key: :fca_number, foreign_key: :fca_number
 
   before_create :populate_question_answers
 
   def self.cache_question_answers(question_answers)
-    cache_key = compute_cache_key(fca_number: question_answers[:fca_number], email: question_answers[:email])
-    KNOWN_REGISTRATION_QUESTIONS.each do |question|
-      REGISTRATION_QUESTION_ANSWERS[cache_key][question.to_sym] = question_answers[question.to_sym] unless question_answers[question.to_sym].nil?
+    the_questions = KNOWN_REGISTRATION_QUESTIONS.filter { |question| question_answers.key?(question) }
+    the_questions.each do |question|
+      cache_key = compute_cache_key(fca_number: question_answers[:fca_number], email: question_answers[:email], question: question)
+      # TODO: consider placing timeouts in config
+      Rails.cache.write(cache_key, question_answers[question.to_sym], expires_in: 1.minute)
     end
   end
 
   def self.compute_cache_key(params)
-    "#{params[:fca_number]}_#{params[:email]}".to_sym
+    "#{params[:fca_number]}_#{params[:email]}_#{params[:question]}"
   end
 
   private
 
   def populate_question_answers
-    KNOWN_REGISTRATION_QUESTIONS.each do |question|
-      firm_principal = Principal.find_by(fca_number: fca_number)
-      cache_key = TravelInsuranceFirm.compute_cache_key(fca_number: fca_number, email: firm_principal.email_address)
-      cached_answers = REGISTRATION_QUESTION_ANSWERS[cache_key(fca_number: fca_number, email: principal.email_address)]
-      instance_eval("#{question} = '#{cached_answers[question.to_sym]}'", __FILE__, __LINE__) unless cached_answers.empty?
+    firm_principal = Principal.find_by(fca_number: fca_number)
 
-      REGISTRATION_QUESTION_ANSWERS.delete(cache_key)
+    KNOWN_REGISTRATION_QUESTIONS.each do |question|
+      cache_key = TravelInsuranceFirm.compute_cache_key(fca_number: fca_number, email: firm_principal.email_address, question: question)
+      cached_answer = Rails.cache.fetch(cache_key) { nil }
+      send("#{question}=", cached_answer)
     end
   end
 end
