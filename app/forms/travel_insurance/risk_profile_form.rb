41b415f7b7762b934f939b02b7634bc97e2157c1
Abraham Kiggundu <abraham@thekiggys.com>
2020-05-11 15:32:31 +0100

    [TP-11365] converted boolean answer questions to bool rather than integer

--- app/forms/travel_insurance/risk_profile_form.rb
+++ app/forms/travel_insurance/risk_profile_form.rb
@@ -5,7 +5,7 @@ class TravelInsurance::RiskProfileForm
                 :risk_profile_approach_question
 
   validates :covered_by_ombudsman_question,
-            inclusion: { in: %w[0 1], message: '%{value} is required' }
+            inclusion: { in: %w[true false], message: '%{value} is required' }
 
   validates :risk_profile_approach_question,
             inclusion: { in: %w[bespoke questionaire neither], message: '%{value} is required' }
