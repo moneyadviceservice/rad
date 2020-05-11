b65a7971877371be4deb4cbcccf56602d04ad816
Abraham Kiggundu <abraham@thekiggys.com>
2020-05-11 15:37:17 +0100

    [TP-11365] minor cleanup and ensured TI firm can create its principle

--- spec/factories/travel_insurance_firm.rb
+++ spec/factories/travel_insurance_firm.rb
@@ -3,5 +3,13 @@ FactoryBot.define do
     fca_number
     registered_name
     approved_at { Time.zone.now }
+
+    transient do
+      create_associated_principle { false }
+    end
+
+    after(:build) do |travel_insurance_firm, evaluator|
+      create(:principal, manually_build_firms: true, fca_number: travel_insurance_firm.fca_number) if evaluator.create_associated_principle
+    end
   end
 end
