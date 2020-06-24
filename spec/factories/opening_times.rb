1513bb5397c2309c90f27d9486b2bad68c664734
Ryan Harkins <silverphp@gmail.com>
2020-06-24 16:10:18 +0100

    Adding OpeningTime factory

---
+++ spec/factories/opening_times.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :opening_time do
+    office
+    weekday_opening_time { "2020-06-17 17:12:38" }
+    weekday_closing_time { "2020-06-17 17:15:38" }
+    saturday_opening_time { "2020-06-17 17:12:38" }
+    saturday_closing_time { "2020-06-17 17:15:38" }
+    sunday_opening_time { "2020-06-17 17:12:38" }
+    sunday_closing_time { "2020-06-17 17:15:38" }
+  end
+end
