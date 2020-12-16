a133f67c26b4f5626794b65c9185e2cbc4b08a94
thenapking <tom.giles@gmail.com>
2020-12-16 11:50:59 +0000

    Only update migration; do not commit schema
    There's been an error in the database.  The rails schema is fine, and we do not want that altered.

--- db/migrate/30191118100176_fix_missing_sequences.rb
+++ db/migrate/30191118100176_fix_missing_sequences.rb
@@ -1,14 +1,14 @@
 class FixMissingSequences < ActiveRecord::Migration[5.2]
   def self.up
-    terms = %w[advisors firms subsidiaries]
+    terms = %w[advisers firms subsidiaries]
     prefixes = %w[lookup_ last_week_lookup_]
 
     prefixes.each do |prefix|
       terms.each do |term|
-        r = execute("SELECT c.relname FROM pg_class c WHERE c.relkind = \'S\' and c.relname = \'#{prefix}#{term.pluralize}_id_seq\' ORDER BY c.relname;")
+        r = execute("SELECT c.relname FROM pg_class c WHERE c.relkind = \'S\' and c.relname = \'#{prefix}#{term}_id_seq\' ORDER BY c.relname;")
         if r.count == 0
           execute("CREATE SEQUENCE #{prefix}#{term.pluralize}_id_seq;")
-          execute("ALTER TABLE #{prefix}#{term.pluralize} ALTER COLUMN id SET DEFAULT nextval(\'#{prefix}#{term.pluralize}_id_seq\');")
+          execute("ALTER TABLE #{prefix}#{term.pluralize} ALTER COLUMN id SET DEFAULT nextval(\'#{prefix}#{term}_id_seq\');")
         end
       end
     end
