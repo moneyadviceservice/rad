11cfa3fe6f885aec3879c7f0656efd38db6e2175
thenapking <tom.giles@gmail.com>
2021-02-04 10:43:29 +0000

    Fix Rubocop

--- spec/presenters/self_service/travel_insurance_status_presenter_spec.rb
+++ spec/presenters/self_service/travel_insurance_status_presenter_spec.rb
@@ -1,10 +1,9 @@
 RSpec.describe SelfService::TravelInsuranceStatusPresenter do
-
   let(:principal) { FactoryBot.create(:principal) }
 
   subject(:presenter) { described_class.new(firm) }
 
-  context 'when the travel_insurance_firm is not publishable' do
+  context 'incomplete firm' do
     let(:firm) { FactoryBot.create(:travel_insurance_firm, completed_firm: false, principal: principal) }
 
     describe '#overall_status' do
@@ -20,50 +19,57 @@ RSpec.describe SelfService::TravelInsuranceStatusPresenter do
     end
   end
 
-  context 'when the travel_insurance_firm is publishable' do
-    let(:firm) { FactoryBot.create(:travel_insurance_firm, completed_firm: true, approved_at: nil) }
+  context 'completed firm' do
+    let(:firm) { FactoryBot.create(:travel_insurance_firm, completed_firm: true) }
 
-    describe '#overall_status' do
-      it 'provides "pending" if the firm is publishable but not approved' do
-        expect(presenter.overall_status).to eq('pending')
+    context 'when the travel_insurance_firm is publishable' do
+      describe '#overall_status' do
+        it 'provides "pending" if the firm is publishable but not approved' do
+          expect(presenter.overall_status).to eq('pending')
+        end
+      end
+
+      describe '#overall_status_icon' do
+        it 'provides "exclamation"' do
+          expect(presenter.overall_status_icon).to eq('tick')
+        end
       end
     end
 
-    describe '#overall_status_icon' do
-      it 'provides "exclamation"' do
-        expect(presenter.overall_status_icon).to eq('tick')
+    context 'when the travel_insurance_firm is publishable' do
+      before do
+        firm.approved_at = Time.zone.now
       end
-    end
-  end
 
-  context 'when the travel_insurance_firm is publishable' do
-    let(:firm) { FactoryBot.create(:travel_insurance_firm, completed_firm: true, approved_at: Time.zone.now) }
-    describe '#overall_status' do
-      it 'provides "published" ' do
-        expect(presenter.overall_status).to eq('published')
+      describe '#overall_status' do
+        it 'provides "published" ' do
+          expect(presenter.overall_status).to eq('published')
+        end
       end
-    end
 
-    describe '#overall_status_icon' do
-      it 'provides "tick"' do
-        expect(presenter.overall_status_icon).to eq('tick')
-      end
-    end
-  end
-
-  context 'when the travel_insurance_firm is hidden' do
-    let(:firm) { FactoryBot.create(:travel_insurance_firm_hidden, completed_firm: true,
-      approved_at: Time.zone.now, hidden_at: Time.zone.now) }
-
-    describe '#overall_status' do
-      it 'provides "hidden" if the travel_insurance_firm is hidden' do
-        expect(presenter.overall_status).to eq('hidden')
+      describe '#overall_status_icon' do
+        it 'provides "tick"' do
+          expect(presenter.overall_status_icon).to eq('tick')
+        end
       end
     end
 
-    describe '#overall_status_icon' do
-      it 'provides "tick"' do
-        expect(presenter.overall_status_icon).to eq('tick')
+    context 'when the travel_insurance_firm is hidden' do
+      before do
+        firm.approved_at = Time.zone.now
+        firm.hidden_at = Time.zone.now
+      end
+
+      describe '#overall_status' do
+        it 'provides "hidden" if the travel_insurance_firm is hidden' do
+          expect(presenter.overall_status).to eq('hidden')
+        end
+      end
+
+      describe '#overall_status_icon' do
+        it 'provides "tick"' do
+          expect(presenter.overall_status_icon).to eq('tick')
+        end
       end
     end
   end
