8c651eb84cf0de4386263c36b2670492a2ff6e9e
thenapking <tom.giles@gmail.com>
2021-02-17 12:53:09 +0000

    Create Rake Task To Update Algolia Index

---
+++ lib/data_migrations/add_new_attributes_to_offerings.rb
@@ -0,0 +1,20 @@
+class AddNewAttributesToOfferings
+  def run_migration
+    Rails.logger.info 'BEGIN Migrating new attributes to Algolia'
+
+    update_algolia_offerings
+
+    Rails.logger.info 'END Migrating new attributes to Algolia'
+  end
+
+  private
+
+  def  update_algolia_offerings
+    TravelInsuranceFirm.all.each do |object|
+      if object.visible_in_directory?
+        tc = object.trip_covers.map(&AlgoliaIndex::TravelInsuranceFirmOfferingSerializer.method(:new))
+        AlgoliaIndex.indexed_travel_insurance_firm_offerings.add_objects(tc)
+      end
+    end
+  end
+end
