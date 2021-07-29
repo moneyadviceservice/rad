44e46866982172388c52f5c4d5d927e365a1d11c
Stephen Rycyk <stephen.rycyk@maps.org.uk>
2021-07-29 16:18:04 +0100

    Tests for FirmStatusCheckJob

--- spec/jobs/firm_status_check_job_spec.rb
+++ spec/jobs/firm_status_check_job_spec.rb
@@ -1,4 +1,75 @@
 RSpec.describe FirmStatusCheckJob do
+  describe 'with api call' do
+    include FcaTestApiCreds
+
+    before { set_env_for_api_calls }
+
+    let(:active_firm) { create(:firm, registered_name: 'Leek United Building Society', fca_number: 100014) }
+    let(:unauthorised_firm) { create(:firm, registered_name: 'Davies Watson', fca_number: 100044) }
+    let(:unknown_firm) { create(:firm, registered_name: 'Beasts', fca_number: 666666) }
+
+    def use_cassette(name)
+      VCR.use_cassette("firm-status-check-job-#{name}") { yield }
+    end
+
+    def perform_now(firm)
+      described_class.perform_now(firm)
+    end
+
+    def check_inactive_firm_creation(count = 1)
+      expect { yield }.to change { InactiveFirm.count }.by count
+    end
+
+    context 'an inactive_firm record' do
+      it 'is created if firm exists with inactive status' do
+        use_cassette(:inactive) do
+          check_inactive_firm_creation { perform_now(unauthorised_firm) }
+        end
+      end
+
+      it 'is created if firm does not exist' do
+        use_cassette(:unknown) do
+          check_inactive_firm_creation { perform_now(unknown_firm) }
+        end
+      end
+
+      it 'is not created if firm exists with active status' do
+        use_cassette(:active) do
+          check_inactive_firm_creation(0) { perform_now(active_firm) }
+        end
+      end
+    end
+
+    context 'field of inactive_firm record' do
+      it 'has correct value if firm exists with inactive status' do
+        use_cassette(:inactive) do
+          perform_now(unauthorised_firm)
+
+          if inactive_firm = InactiveFirm.last
+            expect(inactive_firm.firmable).to eq unauthorised_firm
+
+            expect(inactive_firm.api_status).to eq 'No longer authorised'
+          end
+        end
+      end
+
+      it 'has correct value if firm does not exist' do
+        use_cassette(:unknown) do
+          perform_now(unknown_firm)
+
+          if inactive_firm = InactiveFirm.last
+            expect(inactive_firm.firmable).to eq unknown_firm
+
+            expect(inactive_firm.api_status).to eq 'Not Found'
+          end
+        end
+      end
+    end
+
+    context 'failures when calling api' do
+    end
+  end
+
   describe '#perform' do
     let(:invalid_firm) { double(InvalidFirm) }
     let(:request) { FcaApi::Request }
