bcbbc375262242db95f0b97272d0f2d9301eb968
Ben Lovell <benjamin.lovell@gmail.com>
2025-05-11 14:14:36 +0100

    Configure CSP permitted domain sources

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,8 +9,12 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, :data
   policy.object_src  :none
-  policy.script_src  :self, :https, :unsafe_inline
-  policy.style_src   :self, :https, :unsafe_inline
+
+  policy.script_src  :self, :https, :unsafe_inline,
+    'legacymasassets.blob.core.windows.net',
+    'static.cloudflareinsights.com'
+
+  policy.style_src   :self, :https, :unsafe_inline, 'legacymasassets.blob.core.windows.net'
 
   # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
